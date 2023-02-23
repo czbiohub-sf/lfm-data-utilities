3c996bfeae2c66e51e8d742e4a8dc9ca4a9b4667
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2023-02-22 22:05:57 -0800

    function to parse csv file and return dictionary

--- experiment_metadata_parsing/utils.py
+++ experiment_metadata_parsing/utils.py
@@ -1,10 +1,15 @@
-from typing import List
+from typing import List, Dict
 from pathlib import Path
 from csv import DictReader
 
 
 def get_list_of_per_image_metadata_files(top_level_dir: str) -> List[Path]:
     """Get a list of all the per image metadata in this folder and all its subfolders
+
+    Parameters
+    ----------
+    top_level_dir : str
+        Top level directory path
 
     Returns
     -------
@@ -16,8 +21,34 @@ def get_list_of_per_image_metadata_files(top_level_dir: str) -> List[Path]:
 def get_list_of_experiment_level_metadata_files(top_level_dir: str) -> List[Path]:
     """Get a list of all the experiment-levels metadata in this folder and all its subfolders
 
+    Parameters
+    ----------
+    top_level_dir : str
+        Top level directory path
+
     Returns
     -------
     List[Path]
     """
     return sorted(Path(top_level_dir).glob("**/*exp*.csv"))
+
+
+def parse_csv(filepath: str) -> Dict:
+    """Read the csv file and return a dictionary mapping keys (column headers) to a list of values.
+
+    Parameters
+    ----------
+    filepath : str
+        Path of csv file
+
+    Returns
+    -------
+    Dict
+    """
+    d = {}
+    with open(filepath) as csvfile:
+        reader = DictReader(csvfile)
+        for row in reader:
+            for key in row.keys():
+                d.setdefault(key, []).append(row[key])
+    return d
