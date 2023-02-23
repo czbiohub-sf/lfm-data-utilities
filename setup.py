d8fe9fa9befa212f426f49b7510e56b244ba895a
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2023-02-23 12:50:27 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="LFM Data Utilities",
+    version="1.0",
+    description="Tools for processing data related to the lfm-scope (ulc-malaria-scope)",
+    author="CZ Biohub | Bioengineering",
+    author_email="paul.lebel@czbiohub.org",
+    packages=[],
+    install_requires=["matplotlib>=3.5.3", "tqdm>=4.64.1"],
+)
