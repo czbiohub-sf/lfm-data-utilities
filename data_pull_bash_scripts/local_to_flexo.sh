03e51b44423d2462364b92328d4b2ffe4f71e260
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2023-02-24 14:38:52 -0800

    ensure trailing backslash from user, also properly check if files are different before transferring

--- data_pull_bash_scripts/local_to_flexo.sh
+++ data_pull_bash_scripts/local_to_flexo.sh
@@ -18,10 +18,12 @@ read -p "> [default:login-01.czbiohub.org]: " login_node
 login_node=${login_node:-"login-01.czbiohub.org"}
 echo "Login node: "$login_node""
 
-echo "FROM | Where are files stored on the pi? (press enter to use the default)"
-read -p "> [default:/media/pi/SamsungSSD]: " pi_storage_loc
-pi_storage_loc=${pi_storage_loc:-"/media/pi/SamsungSSD"}
-echo "Will send: "$pi_storage_loc""
+echo "FROM | Where are the files stored? (press enter to use the default)"
+read -p "> [default:/media/pi/SamsungSSD/]: " storage_loc
+storage_loc=${storage_loc:-"/media/pi/SamsungSSD/"}
+[[ "${storage_loc}" != */ ]] && storage_loc="${storage_loc}/"
+echo "Will send: "$storage_loc""
+
 
 echo "TO | Scope name (lowercase)?"
 read -p "> [curiosity/insight/phoenix]: " save_loc
@@ -31,7 +33,7 @@ echo "Saving to "$save_loc""
 echo -e "\nPARAMS"
 echo "- user_name: "$user_name""
 echo "- node: "$login_node""
-echo "- pi storage location: "$pi_storage_loc""
+echo "- pi storage location: "$storage_loc""
 echo "- remote storage location: "$save_loc""
 echo -e "\n"
 echo "Confirm parameters? (y/n)"
@@ -42,7 +44,7 @@ echo ""$user_name"@$login_node":\"""$save_loc"""\""
 
 echo -e "> DRY RUN - files/folder WILL NOT be sent yet"
 echo "=========="
-rsync -rzvhP --exclude ".*" --exclude '*.zip' --exclude "*zstack*" --exclude "*.zarr" --dry-run --stats "$pi_storage_loc" "$user_name"@"$login_node":"$save_loc"
+rsync -rzvichP --exclude ".*" --exclude '*.zip' --exclude "*zstack*" --exclude "*.zarr" --dry-run --stats "$storage_loc" "$user_name"@"$login_node":"$save_loc"
 echo "=========="
 echo -e "> Transfer files? (y/n)"
 read -p "> " confirm
@@ -50,4 +52,4 @@ read -p "> " confirm
 confirmation "$confirm"
 
 echo -e "> Confirmed. Beginning transfer..."
-rsync -rzv --info=progress2 --exclude ".*" --exclude '*.zip' --exclude "*zstack*" --exclude "*.zarr" --stats "$pi_storage_loc" "$user_name"@"$login_node":"$save_loc"
+rsync -rzvc --info=progress2 --exclude ".*" --exclude '*.zip' --exclude "*zstack*" --exclude "*.zarr" --stats "$storage_loc" "$user_name"@"$login_node":"$save_loc"
