76290f5019b87cb4fe0b08798c0b71fbe719a23a
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2023-02-24 12:07:26 -0800

    more general multiprocess load + functions for loading log files + parsing autobrightness vals

--- experiment_metadata_parsing/utils.py
+++ experiment_metadata_parsing/utils.py
@@ -38,6 +38,22 @@ def get_list_of_experiment_level_metadata_files(top_level_dir: str) -> List[Path
     return sorted(Path(top_level_dir).glob("**/*exp*.csv"))
 
 
+def get_list_of_log_files(top_level_dir: str) -> List[Path]:
+    """Get a list of all the logs in this folder
+
+    Parameters
+    ----------
+    top_level_dir : str
+        Top level directory path
+
+    Returns
+    -------
+    List[Path]
+    """
+
+    return sorted(Path(top_level_dir).glob("**/*.log"))
+
+
 def load_csv(filepath: str) -> Dict:
     """Read the csv file and return a dictionary mapping keys (column headers) to a list of values.
 
@@ -60,7 +76,7 @@ def load_csv(filepath: str) -> Dict:
     return d
 
 
-def multiprocess_load_csv(filepaths: List[Path]) -> List[Dict]:
+def multiprocess_load_files(filepaths: List[Path], fn: callable) -> List:
     """Wraps parse_csv with multiprocessing. Takes a list of filepaths to load.
 
     Parameters
@@ -69,10 +85,86 @@ def multiprocess_load_csv(filepaths: List[Path]) -> List[Dict]:
 
     Returns
     -------
-    List[Tuple[Dict]]
-        A list of dictionaries mapping columns to a list of their values
+    List[Interior type depends on output of callable]
     """
 
     with Pool() as pool:
-        data = list(tqdm(pool.imap(load_csv, filepaths), total=len(filepaths)))
+        data = list(tqdm(pool.imap(fn, filepaths), total=len(filepaths)))
     return data
+
+
+def multiprocess_load_csv(filepaths: List[Path]) -> List[Dict]:
+    """Multiprocess load csv files
+
+    Parameters
+    ----------
+    filepaths: List[str]
+
+    Returns
+    -------
+    List[Dict]
+        A list of dictionaries mapping columns to a list of their values
+    """
+
+    return multiprocess_load_files(filepaths, load_csv)
+
+
+def multiprocess_load_log(filepaths: List[Path]) -> List[List[str]]:
+    """Multiprocess load log files
+
+    Parameters
+    ----------
+    filepaths: List[str]
+
+    Returns
+    -------
+    List[List[str]]
+        For each file, a list of strings (lines) from that file, separated by newline.
+    """
+
+    return multiprocess_load_files(filepaths, load_log_file)
+
+
+def load_log_file(filepath: str) -> List[str]:
+    """Get lines, split by newlines, from the log file
+
+    Parameters
+    ----------
+    filepath : str
+        Path of log file
+
+    Returns
+    -------
+    List[str]
+        List of lines from the log file
+    """
+
+    lines = []
+
+    with open(filepath) as f:
+        lines = f.read().splitlines()
+
+    return lines
+
+
+def get_autobrightness_vals_from_log(lines: List[str]) -> List[float]:
+    """Parse through log file lines and return a list of autobrightness values
+
+    Parameters
+    ----------
+    lines: List[str]
+        Lines extracted from the log file (i.e run `load_log_file` and pass in the output of that function)
+
+    Returns
+    -------
+    List[float]
+    """
+
+    # Get relevant lines
+    autobrightness_vals = [l for l in lines if "Mean pixel val" in l]
+
+    # Parse relevant lines and get the autobrightness value out
+    autobrightness_vals = [l[: l.find("[")].strip() for l in autobrightness_vals]
+    autobrightness_vals = [float(l.split(" ")[-1][:-1]) for l in autobrightness_vals]
+
+    return autobrightness_vals
