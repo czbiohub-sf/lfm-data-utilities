9b86f5b2121aa32fb24068c7cac80f09546c14e6
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2023-03-24 16:59:05 -0700

    mp make_video

--- lfm_data_utilities/image_processing/make_video.py
+++ lfm_data_utilities/image_processing/make_video.py
@@ -1,13 +1,18 @@
 import sys
-
+from functools import partial
 from lfm_data_utilities.utils import *
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"usage: {sys.argv[0]} <path folders>")
+    if len(sys.argv) != 3:
+        print(f"usage: {sys.argv[0]} <path folders> <path to save videos (note: folder will be created if it doesn't exist already)>")
         sys.exit(1)
 
     path_to_runset = sys.argv[1]
+    path_to_save = sys.argv[2]
     datasets = load_datasets(path_to_runset)
     valid_datasets = [d for d in datasets if d.successfully_loaded]
-    make_video(valid_datasets[0], Path("vids"))
+
+    make_video_with_path = partial(make_video, save_dir=path_to_save)
+    print("Generating videos...")
+    with Pool() as pool:
+        tqdm(pool.imap(make_video_with_path, valid_datasets), total=len(valid_datasets))
