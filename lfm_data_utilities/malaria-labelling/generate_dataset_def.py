41fbd95673f600ab0e5a4a5b3e88a34ec1fee4b6
Axel Jacobsen <axelnj44@gmail.com>
2023-03-31 13:47:14 -0700

    convert labels if req'd

--- lfm_data_utilities/malaria-labelling/generate_dataset_def.py
+++ lfm_data_utilities/malaria-labelling/generate_dataset_def.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 
 import sys
+import time
 
+from tqdm import tqdm
 from pathlib import Path
 from typing import Tuple, List, Dict
 
@@ -29,16 +31,65 @@ dataset_paths:
 
 def class_names_from_classes_dot_txt(path_to_classes_dot_txt: Path) -> List[str]:
     with open(str(path_to_classes_dot_txt), "r") as f:
-        return [s.strip() for s in f.readlines()]
+        return [s.strip() for s in f.readlines() if s != ""]
 
 
-def gen_dataset_def(path_to_runset_folder: Path):
+def convert_labels_folder_class_ordering(run_set_path: Path, from_classes: List[str], to_classes: List[str]) -> Path:
+    """ If we encounter a folder that has a different ordering of classes
+    than our master ordering, we must correct it.
+
+    This function will go through every label and swap it for the correct one.
+
+    It will return a path to the new, corrected labels.
+    """
+    # first, we need to make sure that from_classes is a strict subset of to_classes
+    if set(from_classes) - set(to_classes) != set():
+        raise ValueError(
+            f"there are classes in from_classes that are not in to_classes; conversion failure "
+            f"from_classes = {from_classes}, to_classes = {to_classes}, path = {run_set_path}"
+        )
+
+    if not (run_set_path / "labels").exists():
+        raise ValueError(f"director {(run_set_path / 'labels')} doesn't exist")
+
+    (run_set_path / "converted_labels").mkdir(exist_ok=True)
+
+    for file in (run_set_path / "labels").iterdir():
+        with open(file, "r") as f:
+            contents = f.read().strip()
+
+        # can probably do a r+ above, but I don't want to deal with seeking and
+        # those complications - just reopen the file
+        with open(run_set_path / "converted_labels" / file.name, "w") as g:
+            for line in contents.split("\n"):
+                contents = line.split(" ")
+
+                if contents[0].isnumeric():
+                    class_index = int(contents[0])
+                    to_class = to_classes.index(from_classes[class_index])
+                else:
+                    class_index = contents[0]
+                    to_class = to_classes.index(class_index)
+
+                g.write(" ".join([str(to_class), *contents[1:]]) + '\n')
+
+    with open("old_classes.txt", "w") as f:
+        for class_id in from_classes:
+            f.write(f"{class_id}\n")
+
+    with open(run_set_path / "classes.txt", "w") as f:
+        for class_id in to_classes:
+            f.write(f"{class_id}\n")
+
+    return run_set_path / "converted_labels"
+
+def gen_dataset_def(path_to_runset_folder: Path, verbose=False):
     folders = [Path(p).parent for p in path_to_runset_folder.glob("./**/images")]
 
     dataset_paths: Dict[str, Dict[str, str]] = dict()
-    all_classes = None
 
-    for i, folder_path in enumerate(folders):
+    iterator = tqdm(enumerate(folders)) if verbose else enumerate(folders)
+    for i, folder_path in iterator:
         # check classes
         images_path = folder_path / "images"
         label_path = folder_path / "labels"
@@ -53,21 +104,14 @@ def gen_dataset_def(path_to_runset_folder: Path):
         classes = (
             class_names_from_classes_dot_txt(classes_path)
             if classes_path.exists()
-            else CLASSES
+            else CLASSES  # if no classes.txt exists, assume that it has been default-labelled healthy
         )
 
-        if all_classes is None:
-            # arbitrarily take the first `classes.txt`
-            # maybe in the future, we convert the
-            # classes.txt, but for now this is OK
-            all_classes = classes
-        elif all_classes != classes:
-            raise ValueError(
-                "found a runset folder that has a `classes.txt` that doesn't match "
-                f"other runset folders; check {folder_path}\n"
-                f"previously found classes: {all_classes}\n"
-                f"classes for this runset folder: {classes}"
-            )
+        if CLASSES != classes:
+            print(f"converting {folder_path} from {classes} to {CLASSES}...")
+            t0 = time.perf_counter()
+            label_path = convert_labels_folder_class_ordering(folder_path, classes, CLASSES)
+            print(f"converted ({time.perf_counter() - t0:.3f} s)")
 
         dataset_paths[folder_path.name] = {
             "image_path": str(images_path),
@@ -75,7 +119,7 @@ def gen_dataset_def(path_to_runset_folder: Path):
         }
 
     dataset_defs = {
-        "class_names": all_classes,
+        "class_names": CLASSES,
         "dataset_split_fractions": {"train": 0.75, "test": 0.20, "val": 0.05},
         "dataset_paths": dataset_paths,
     }
@@ -98,4 +142,4 @@ if __name__ == "__main__":
     if not path_to_runset.exists():
         raise ValueError(f"{str(path_to_runset)} doesn't exist")
 
-    gen_dataset_def(path_to_runset)
+    gen_dataset_def(path_to_runset, verbose=True)
