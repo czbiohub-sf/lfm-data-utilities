96e7405f0c929bfaef785dbf323ff9c660a4f7c9
Michelle Khoo <michelle.khoo@czbiohub.org>
2023-04-04 14:51:35 -0700

    Updated dependencies

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="lfm_data_utilities",
+    name="LFM Data Utilities",
     version="1.0",
     description="Tools for processing data related to the lfm-scope (ulc-malaria-scope)",
     author="CZ Biohub | Bioengineering",
     author_email="paul.lebel@czbiohub.org",
-    packages=find_packages(),
+    packages=[],
     install_requires=[
-        "AllanTools==2019.9",
         "matplotlib>=3.5.3",
-        "numpy>=1.24.2",
-        "opencv-python>=4.7.0.72",
         "tqdm>=4.64.1",
+        "AllanTools==2019.9",
+        "numpy==1.24.2",
+        "matplotlib==3.7.0",
         "opencv-python>=4.7.0.72",
-        "zarr==2.13.3"
+        "autofocus @ git+https://github.com/czbiohub/ulc-malaria-autofocus@main",
+        "torch>=1.13.1",
     ],
 )
