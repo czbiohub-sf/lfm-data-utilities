56f56751306dc0d558277dfb95bc94f3128a60ea
Michelle Khoo <michelle.khoo@login-01.clusternet>
2023-04-04 12:35:54 -0700

    Script is working

--- ssaf_rerun_bash_scripts/ssaf_rerun_all.py
+++ ssaf_rerun_bash_scripts/ssaf_rerun_all.py
@@ -3,86 +3,77 @@ import os
 import torch
 import sys
 
+from autofocus.infer import load_model_for_inference, infer, ImageLoader
+from zipfile import BadZipFile
 from time import perf_counter
 from tqdm import tqdm
 
-
-class Rerunner():
-    def __init__(self, infer, model_loader, image_loader):
-        self.infer = infer
-        self.model_loader = model_loader
-        self.image_loader = image_loader
-
-        self.file_format = f'{data_dir}/*-*-*-*/*-*-*-*_/*.zip'
     
-    def get_files(self, data_dir):
-        a = perf_counter()
-        files = glob.glob(self.file_format)
+def get_files(data_dir):
+    file_format = f'{data_dir}/*-*-*-*/*-*-*-*_/*.zip'
         
-        print(files)
-        print(f"Number of files: {len(files)}")
+    a = perf_counter()
+    files = glob.glob(file_format)
         
-        b = perf_counter()
-        print(f"Get files: {b-a} s")
+    print(files)
+    print(f"Number of files: {len(files)}")
+        
+    b = perf_counter()
+    print(f"Get files: {b-a} s")
 
-        return(files)
+    return(files)
 
-    def load_model(self, model_dir):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print(f"Using {device}")
-        model = self.model_loader(model_dir, device)
+def load_model(model_dir):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using {device}")
+    model = load_model_for_inference(model_dir, device)
 
-        return model
+    return model
 
-    def process_files(self, files, model, output_dir):
-        for file in files:
-            images = (self.image_loader.load_zarr_data(file))
-            basename = os.path.basename(file)
-            output_file = f"{output_dir}/{basename.removesuffix('.zip')}__ssaf.txt"
+def process_files(files, model, output_dir):
+    for file in files:
+        basename = os.path.basename(file)
+        
+        try:
+            images = (ImageLoader.load_zarr_data(file))
+        except BadZipFile:
+            print(f"Skipping {basename}: BadZipFile")
+            continue
 
-            if output_file.is_file():
-                print(f"Skipping {basename}, already processed")
-            else:
-                print(f"Started processing {basename}")
+        output_file = f"{output_dir}/{basename.removesuffix('.zip')}__ssaf.txt"
 
-                c = perf_counter()
-                try:
-                    with open(output_file, 'w') as file:
-                        for res in tqdm(infer(model, images)):
-                            file.write(f"{res}\n")
-                except BadZipFile:
-                    print(f"BadZipFile: {basename}")
-                else:
-                    d = perf_counter()
-                    print(f"Finished writing {basename} SSAF data in {d-c} s")
+        if os.path.exists(output_file):
+            print(f"Skipping {basename}: Already processed")
+            continue
+            
+        print(f"Started processing {basename}")
 
+        c = perf_counter()
+        with open(output_file, 'w') as file:
+            for res in tqdm(infer(model, images)):
+                file.write(f"{res}\n")
+        d = perf_counter()
+        print(f"Finished writing {basename} SSAF data in {d-c} s")
+        return
 
-    def rerun(self, scope_dir, model_dir, output_dir):
-        model = load_model(model_dir)
-        files = get_files(scope_dir)
-        process_files(files, model, output_dir)
+def rerun(scope_dir, model_dir, output_dir):
+    model = load_model(model_dir)
+    files = get_files(scope_dir)
+    process_files(files, model, output_dir)
 
 
 if __name__ == "__main__":
 
     print(sys.argv)
     try:
-        ssaf_repo = sys.argv[1]
-        scope_folder = sys.argv[2]
-        model_file = sys.argv[3]
-        output_folder = sys.argv[4]
+        scope_folder = sys.argv[1]
+        model_file = sys.argv[2]
+        output_folder = sys.argv[3]
     except IndexError:
         raise Exception(
-            "Expected format 'python3 ssaf_rerun_all.py <path to ulc-malaria-autofocus repo> <path to scope folder> <path to model .pth file> <path to output folder>'"
+            "Expected format 'python3 ssaf_rerun_all.py <path to scope folder> <path to model .pth file> <path to output folder>'"
         )
 
-    try: 
-        sys.path.append(os.path.abspath(ssaf_repo))
-        from infer import load_model_for_inference, infer, ImageLoader
-    except Exception as e:
-        print(e)
-    
-    SSAF_rerunner = Rerunner(load_model_for_inference, infer, ImageLoader)
-    SSAF_rerunner.rerun(scope_folder, model_file, output_folder)
+    rerun(scope_folder, model_file, output_folder)
 
     print("Finished processing")
