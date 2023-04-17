8009d25eaf336d5dc729775e26f29ba85e069608
Axel Jacobsen <axelnj44@gmail.com>
2023-04-17 13:25:20 -0700

    add args to sanity checker

--- lfm_data_utilities/malaria-labelling/dataset_label_sanity_checker.sh
+++ lfm_data_utilities/malaria-labelling/dataset_label_sanity_checker.sh
@@ -2,13 +2,29 @@
 
 count=0
 
-find . -type d -name "images" > /tmp/img_dirs.txt
+# argument 1 should be the parent directory to search from
+# argument 2 should be the name of the labels dir - default to "labels"
+
+# if there are not 1 or two arguments, print usage and exit
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+  echo "Usage: $0 <parent_dir> [labels_dir]"
+  exit 1
+fi
+
+parent=$1
+
+if [ $# -eq 2 ]; then
+  labels_dir_name=$2
+else
+  labels_dir_name="labels"
+fi
+
+find "$parent" -type d -name "images" > /tmp/img_dirs.txt
 
 while read images_dir; do
   # Check if the corresponding labels directory exists
-  labels_dir="$images_dir/../labels"
+  labels_dir="$images_dir/../$labels_dir_name"
   if [ ! -d "$labels_dir" ]; then
-    echo "dANG"
     continue
   fi
 
