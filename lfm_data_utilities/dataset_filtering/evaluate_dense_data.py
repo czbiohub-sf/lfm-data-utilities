d55b9f68d020544091d426d6c68d3244efe2d05f
Axel Jacobsen <axelnj44@gmail.com>
2023-05-03 13:32:13 -0700

    export to csv file

--- lfm_data_utilities/dataset_filtering/evaluate_dense_data.py
+++ lfm_data_utilities/dataset_filtering/evaluate_dense_data.py
@@ -3,11 +3,11 @@
 import csv
 import argparse
 
+from tqdm import tqdm
 from pathlib import Path
-from typing import Dict, cast
+from typing import List, Dict, cast, Optional
 
 from ruamel.yaml import YAML
-from lfm_data_utilities import utils
 
 from lfm_data_utilities.dataset_filtering import evaluators as ev
 
@@ -65,6 +65,11 @@ if __name__ == "__main__":
         default=0.9,
         help="percent of results (in [0,1]) that must succeed for a pass (default 0.9)",
     )
+    parser.add_argument(
+        "--csv-output",
+        type=Path,
+        help="output to csv instead of stdout - if not provided (default) just print to stdout",
+    )
 
     args = parser.parse_args()
 
@@ -76,25 +81,56 @@ if __name__ == "__main__":
             f"{args.path_to_dense_data_dir} does not exist or is not a dir"
         )
 
-    with utils.timing_context_manager("evals"):
-        for data_file in args.path_to_dense_data_dir.rglob("data.csv"):
-            metadata = load_metadata_for_dataset_path(data_file.parent / "meta.yml")
+    header: Optional[str] = None
+    rows: List[str] = []
 
-            try:
-                target_flowrate = get_flowrate(metadata["target_flowrate"])
-            except KeyError:
-                # TODO a *lot* of them don't record experiment metadata
-                target_flowrate = 7.58
+    data_files = list(args.path_to_dense_data_dir.rglob("data.csv"))
 
-            evaluators = ev.EvaluatorCollection(
-                ev.SSAFEvaluator(args.ssaf_failrate, args.ssaf_step),
-                ev.FlowrateEvaluator(
-                    failrate=args.flowrate_failrate,
-                    flowrate=target_flowrate,
-                    relative_range=0.3,
-                    flowrate_confidence_threshold=0.3,
-                ),
-                ev.YOGOEvaluator(),
-            )
-            eval_data_csv(data_file, evaluator=evaluators)
-            print("\n".join(evaluators.to_csv()))
+    for data_file in tqdm(
+        data_files, total=len(data_files), disable=(args.csv_output is None)
+    ):
+        metadata = load_metadata_for_dataset_path(data_file.parent / "meta.yml")
+
+        try:
+            target_flowrate = get_flowrate(metadata["target_flowrate"])
+        except KeyError:
+            # TODO a *lot* of them don't record experiment metadata
+            # Is this the right flowrate? Maybe we should just detect
+            # when flowrate varies too much? Should we use ewma for this?
+            # Should we use ewma for focus?
+            target_flowrate = 7.58
+
+        evaluators = ev.EvaluatorCollection(
+            ev.SSAFEvaluator(args.ssaf_failrate, args.ssaf_step),
+            ev.FlowrateEvaluator(
+                failrate=args.flowrate_failrate,
+                flowrate=target_flowrate,
+                relative_range=0.3,
+                flowrate_confidence_threshold=0.3,
+            ),
+            ev.YOGOEvaluator(),
+        )
+        eval_data_csv(data_file, evaluator=evaluators)
+
+        ev_header, row = evaluators.to_csv()
+
+        # these 'if's below are a bit gross
+        if header is None:
+            header = f"dataset,{ev_header}"
+            if not args.csv_output is not None:
+                print(header)
+
+        if args.csv_output is not None:
+            rows.append(f"{data_file.parent.name},{row}")
+        else:
+            print(f"{data_file.parent.name},{row}")
+
+    if header is None:
+        raise ValueError("No data found")
+
+    if args.csv_output:
+        rows.sort()
+        with open(args.csv_output, "w") as f:
+            f.write(header + "\n")
+            for row in rows:
+                f.write(row + "\n")
