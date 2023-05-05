e6e61f922fb735de7c61e702303433288d4cd251
Axel Jacobsen <axelnj44@gmail.com>
2023-05-05 15:39:50 -0700

    attempt to generate more confusion matricies, but permission errors! agh!

--- lfm_data_utilities/model_evaluations/autofocus/dataset_similarity_confusion.py
+++ lfm_data_utilities/model_evaluations/autofocus/dataset_similarity_confusion.py
@@ -18,6 +18,7 @@ from PIL import Image
 from tqdm import tqdm
 from pathlib import Path
 from typing import Optional
+from functools import partial
 
 from lfm_data_utilities import utils
 
@@ -28,16 +29,15 @@ os.environ["MPLBACKEND"] = "Agg"
 os.environ["QT_QPA_PLATFORM"] = "offscreen"
 
 
-def generate_confusion_matrix(folder: Path, output: Optional[Path] = None):
+def generate_confusion_matrix(folder: Path, output_dir: Path = Path(".")):
     # Get list of images in folder
     images = list(folder.glob("*.png"))
     images.sort()
 
     # Load images (woa! we can fit them in ram!)
-    with utils.timing_context_manager(f"loading {len(images)} images!"):
-        imgs = []
-        for img in images:
-            imgs.append(cv2.imread(str(img), cv2.IMREAD_GRAYSCALE))
+    imgs = []
+    for img in images:
+        imgs.append(cv2.imread(str(img), cv2.IMREAD_GRAYSCALE))
 
     indices = [(i,j) for i in range(len(imgs)) for j in range(i+1, len(imgs))]
 
@@ -55,38 +55,66 @@ def generate_confusion_matrix(folder: Path, output: Optional[Path] = None):
     utils.multithread_map_unordered(
         indices,
         update_confusion_matrix,
-        verbose=True,
-   )
+        verbose=False,
+    )
 
-    # Save confusion matrix as image
-    np.save("confusion_matrix.npy", conf)
+    # directory structure here is
+    # ssaf_data_dir/
+    #   training_data/
+    #       0/
+    #       1/
+    #       2/
+    #       ...
+    # we want to save the confusion matricies in ssaf_data_dir
+    # and folder is 0/ or 1/ or ...
+    folder_name = folder.name  # this is the step
+
+    # Save confusion matrix
+    np.save(str(output_dir / f"confusion_matrix_{folder_name}.npy"), conf)
+
+    # and the confusion matrix image
     im = Image.fromarray(conf)
-    newname = args.output.parent / f"{args.output.stem}_confusion_matrix.png"
-    im.save(str(newname.with_suffix(".png")))
+    im.save(str(output_dir / f"confusion_matrix_{folder_name}.png"))
 
     # Plot confusion matrix
     plt.rcParams["figure.figsize"] = (12,12)
     plt.imshow(conf, cmap="gray")
     plt.colorbar()
-    plt.title(f"{folder.parent.parent.name}")
+    plt.title(f"{folder.name}")
     plt.gca().xaxis.tick_top()
     plt.xlabel("Image")
     plt.ylabel("Image")
     plt.tight_layout()
 
     if args.output:
-        plt.savefig(args.output.with_suffix(".png"), dpi=800)
+        plt.savefig((output_dir / "map").with_suffix(".png"), dpi=800)
     else:
         plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("folder", type=Path, help="Folder containing images")
+    data_source = parser.add_mutually_exclusive_group(required=True)
+    data_source.add_argument("-f", "--folder", type=Path, help="path to of images")
+    data_source.add_argument(
+        "--ssaf-data-dir",
+        type=Path,
+        help=(
+            "path to folder of training data - will find all `training_data` dirs"
+            "to find images, and will create a graph per each `training_data` dir"
+        )
+    )
     parser.add_argument("-o", "--output", type=Path, help="Output file name")
     args = parser.parse_args()
 
-    folder = args.folder
-    if not folder.exists():
-        raise ValueError(f"Folder {folder} does not exist")
-
-    generate_confusion_matrix(folder, args.output)
+    if args.folder:
+        generate_confusion_matrix(args.folder, args.output)
+    else:
+        for training_data_dir in args.ssaf_data_dir.rglob("training_data"):
+            step_folders = [step_folder for step_folder in training_data_dir.iterdir() if step_folder.is_dir()]
+            output = training_data_dir / "confusion_matricies"
+            output.mkdir(exist_ok=True)
+            utils.multiprocess_fn(
+                step_folders,
+                partial(generate_confusion_matrix, output_dir=output),
+                ordered=False,
+            )
