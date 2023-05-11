5f056e39e95ae2599b65b590dcdf02a1d5993838
Axel Jacobsen <axelnj44@gmail.com>
2023-05-10 17:15:52 -0700

    dash working

--- lfm_data_utilities/model_evaluations/yogo/output_analysis/pl2.py
+++ lfm_data_utilities/model_evaluations/yogo/output_analysis/pl2.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python3
 
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, callback, Input, Output
 
 import argparse
 import numpy as np
 
 from PIL import Image
 from pathlib import Path
+from typing import Optional
 
 import plotly.express as px
 import plotly.subplots as sp
@@ -15,89 +16,12 @@ import plotly.graph_objects as go
 import yogo
 
 
-def plot_heatmap(image_data, objectness_data, class_data, scale=0.8):
-    # Calculate the total aspect ratio and the proportion for each subplot
-    image_aspect = image_data.shape[1] / image_data.shape[0]
-    heatmap_aspect = objectness_data.shape[1] / objectness_data.shape[0]
-
-    total_aspect = image_aspect + heatmap_aspect
-    image_proportion = image_aspect / total_aspect
-
-    # Create subplots with 1 row and 2 columns
-    spacing = 0.04
-    fig = sp.make_subplots(
-        rows=1,
-        cols=2,
-        subplot_titles=["image", "objectness", "", "class predictions"],
-        specs=[[{"type": "image"}, {"type": "heatmap"}], [None, {"type": "heatmap"}]],
-        horizontal_spacing=spacing,
-        vertical_spacing=spacing,
+def plot_heatmap(yogo_output_array: np.ndarray, z_data: Optional[np.ndarray] = None):
+    return px.imshow(
+        yogo_output_array,
+        # showscale=False,
+        # hoverinfo=z_data if z_data is not None else "z",
     )
-
-    fig.add_trace(px.imshow(image_data).data[0], row=1, col=1)
-
-    fig.add_trace(
-        go.Heatmap(
-            z=objectness_data,
-            showscale=False,
-            colorscale="Viridis",
-            hoverinfo="z",
-            xaxis="x2",
-            yaxis="y2",
-        ),
-        row=2,
-        col=2,
-    )
-
-    fig.add_trace(
-        go.Heatmap(
-            z=class_data,
-            showscale=False,
-            colorscale="Viridis",
-            hoverinfo="z",
-            xaxis="x2",
-            yaxis="y2",
-        ),
-        row=1,
-        col=2,
-    )
-
-    # Update layouts so the heatmaps ahve same heights, maintain aspect ratios,
-    # and in general make the plot pretty
-    fig.update_layout(
-        yaxis_visible=False,
-        yaxis_showticklabels=False,
-        xaxis_visible=False,
-        xaxis_showticklabels=False,
-        yaxis2_visible=False,
-        yaxis2_showticklabels=False,
-        xaxis2_visible=False,
-        xaxis2_showticklabels=False,
-        yaxis3_visible=False,
-        yaxis3_showticklabels=False,
-        xaxis3_visible=False,
-        xaxis3_showticklabels=False,
-        coloraxis_showscale=False,
-        xaxis=dict(domain=[0, image_proportion]),
-        xaxis2=dict(domain=[image_proportion + spacing, 1]),
-        xaxis3=dict(domain=[image_proportion + spacing, 1]),
-        yaxis=dict(domain=[0, 0.5]),
-        yaxis2=dict(domain=[0, 0.5]),
-        yaxis3=dict(domain=[0.5 + spacing, 1]),
-        width=int(
-            (
-                1032
-                + objectness_data.shape[1]
-                * (image_data.shape[0] / objectness_data.shape[0])
-            )
-            * scale
-        ),
-        height=int(772 * 2 * scale),
-        margin=dict(l=20, r=20, t=20, b=20),
-    )
-
-    fig.show()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -127,10 +51,26 @@ if __name__ == "__main__":
     class_confidences = np.max(classifications, axis=0)
 
     app = Dash(__name__)
-    app.layout = html.Div(
-        [
-            html.H4("YOGO Output Analysis"),
-            dcc.Graph(id="graph"),
-            html.Button("swap", id="submit-val", n_clicks=0),
-        ]
+
+    app.layout = html.Div([
+        html.Div(children="YOGO model output analysis"),
+        html.Div(children=f"model: {args.pth_path}"),
+        html.Div(children=f"image: {args.image_path}"),
+        dcc.Graph(figure=px.imshow(bbox_image)),
+        dcc.Graph(figure={}, id='YOGO-output'),
+        dcc.RadioItems(options=['objectness', 'classification', 'bbox'], value='objectness', id='YOGO-output-selector'),
+    ])
+
+    @callback(
+        Output(component_id='YOGO-output', component_property='figure'),
+        Input(component_id='YOGO-output-selector', component_property='value'),
     )
+    def update_yogo_output(value):
+        if value == 'objectness':
+            return plot_heatmap(objectness_heatmap)
+        elif value == 'classification':
+            return plot_heatmap(class_confidences)
+        elif value == 'bbox':
+            raise NotImplementedError
+
+    app.run_server(debug=True, use_reloader=True)
