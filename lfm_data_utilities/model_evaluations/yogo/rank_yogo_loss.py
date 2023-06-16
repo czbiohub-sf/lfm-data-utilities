7c23c8b0b441bb97ff8b29b9f4b152bc16acb83f
Axel Jacobsen <axelnj44@gmail.com>
2023-06-16 10:43:52 -0700

    working!

--- lfm_data_utilities/model_evaluations/yogo/rank_yogo_loss.py
+++ lfm_data_utilities/model_evaluations/yogo/rank_yogo_loss.py
@@ -7,26 +7,24 @@ Goal: Run this script with YOGO over all of our labelled data and rank by loss!
 - rank by loss type (classification v. IoU v. objectness)
 """
 
-
-from typing import Tuple
-
-from pathlib import Path
-
 import os
 import torch
+import argparse
+import pandas as pd
 
 from tqdm import tqdm
+from typing import Tuple
+from pathlib import Path
+
+from typing import Any, List, Dict, Union, Tuple, Optional, MutableMapping
 
 from torch.utils.data import Dataset, ConcatDataset, DataLoader
 
-from typing import Dict, Tuple, Optional, Any, MutableMapping
-
+from yogo.model import YOGO
+from yogo.yogo_loss import YOGOLoss
 from yogo.data.blobgen import BlobDataset
-from yogo.data.dataset import ObjectDetectionDataset
 from yogo.data.dataloader import split_dataset
 from yogo.data.dataset_description_file import load_dataset_description
-
-
 from yogo.data.dataset import ObjectDetectionDataset, label_file_to_tensor
 
 
@@ -44,13 +42,12 @@ class ObjectDetectionDatasetWithPaths(ObjectDetectionDataset):
         return image, labels, image_path, label_path
 
 
-def get_datasets(
+def get_dataset(
     dataset_description_file: str,
     Sx: int,
     Sy: int,
-    split_fractions_override: Optional[Dict[str, float]] = None,
     normalize_images: bool = False,
-) -> MutableMapping[str, Dataset[Any]]:
+) -> Dataset[Any]:
     dataset_description = load_dataset_description(dataset_description_file)
     # can we speed this up? multiproc dataset creation?
     full_dataset: ConcatDataset[ObjectDetectionDataset] = ConcatDataset(
@@ -61,46 +58,9 @@ def get_datasets(
             Sy,
             normalize_images=normalize_images,
         )
-        for dsp in tqdm(dataset_description.dataset_paths)
+        for dsp in tqdm(dataset_description.dataset_paths, desc="loading datasets")
     )
-
-    if dataset_description.test_dataset_paths is not None:
-        test_dataset: ConcatDataset[ObjectDetectionDataset] = ConcatDataset(
-            ObjectDetectionDatasetWithPaths(
-                dsp["image_path"],
-                dsp["label_path"],
-                Sx,
-                Sy,
-                normalize_images=normalize_images,
-            )
-            for dsp in tqdm(dataset_description.test_dataset_paths)
-        )
-        split_datasets: MutableMapping[str, Dataset[Any]] = {
-            "train": full_dataset,
-            **split_dataset(test_dataset, dataset_description.split_fractions),
-        }
-    else:
-        split_datasets = split_dataset(
-            full_dataset, dataset_description.split_fractions
-        )
-
-    # hardcode the blob agumentation for now
-    # this should be moved into the dataset description file
-    if dataset_description.thumbnail_augmentation is not None:
-        # some issue w/ Dict v Mapping TODO come back to this
-        bd = BlobDataset(
-            dataset_description.thumbnail_augmentation,  # type: ignore
-            Sx=Sx,
-            Sy=Sy,
-            n=8,
-            length=len(split_datasets["train"]) // 4,  # type: ignore
-            blend_thumbnails=True,
-            thumbnail_sigma=2,
-            normalize_images=normalize_images,
-        )
-        split_datasets["train"] = ConcatDataset([split_datasets["train"], bd])
-
-    return split_datasets
+    return full_dataset
 
 
 def collate_batch(batch):
@@ -115,36 +75,84 @@ def get_dataloader(
     batch_size: int,
     Sx: int,
     Sy: int,
-    training: bool = True,
-    preprocess_type: Optional[str] = None,
-    vertical_crop_size: Optional[float] = None,
-    resize_shape: Optional[Tuple[int, int]] = None,
-    split_fractions_override: Optional[Dict[str, float]] = None,
     normalize_images: bool = False,
-) -> Dict[str, DataLoader]:
-    split_datasets = get_datasets(
+) -> DataLoader:
+    full_dataset = get_dataset(
         dataset_descriptor_file,
         Sx,
         Sy,
-        split_fractions_override=split_fractions_override,
         normalize_images=normalize_images,
     )
 
     num_workers = min(len(os.sched_getaffinity(0)) // 2, 32)
 
-    d = dict()
-    for designation, dataset in split_datasets.items():
-        d[designation] = DataLoader(
-            dataset,
-            shuffle=True,
-            drop_last=False,
-            pin_memory=True,
-            batch_size=batch_size,
-            persistent_workers=num_workers > 0,
-            multiprocessing_context="spawn" if num_workers > 0 else None,
-            # optimal # of workers? >= 32
-            num_workers=num_workers,  # type: ignore
-            generator=torch.Generator().manual_seed(111111),
-            collate_fn=collate_batch,
-        )
+    d = DataLoader(
+        full_dataset,
+        shuffle=True,
+        drop_last=False,
+        pin_memory=True,
+        batch_size=batch_size,
+        persistent_workers=num_workers > 0,
+        multiprocessing_context="spawn" if num_workers > 0 else None,
+        # optimal # of workers? >= 32
+        num_workers=num_workers,  # type: ignore
+        generator=torch.Generator().manual_seed(111111),
+        collate_fn=collate_batch,
+    )
     return d
+
+
+def get_loss_df(dataset_descriptor_file, path_to_pth) -> pd.DataFrame:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    Y_loss = YOGOLoss(
+        no_obj_weight=1,
+        iou_weight=1,
+        classify_weight=1,
+        label_smoothing=0,
+    ).to(device)
+
+    net, net_cfg = YOGO.from_pth(path_to_pth)
+    net.to(device)
+
+    dataloader = get_dataloader(
+        dataset_descriptor_file,
+        batch_size=1,
+        Sx=net.Sx,
+        Sy=net.Sy,
+        normalize_images=net_cfg["normalize_images"],
+    )
+
+    values: List[Dict[str, Union[int, float]]] = []
+
+    for batch in tqdm(dataloader, desc="calculating loss"):
+        inputs, labels, image_paths, label_paths = batch
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        loss, loss_components = Y_loss(net(inputs), labels)
+        values.append(
+            {
+                "image_path": image_paths.pop(),
+                "label_path": label_paths.pop(),
+                "total_loss": loss.item(),
+                **loss_components,
+            }
+        )
+
+    return pd.DataFrame(values)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="rank YOGO loss", allow_abbrev=False)
+    parser.add_argument(
+        "dataset_descriptor_file",
+        type=Path,
+        help="path to yml dataset descriptor file",
+    )
+    parser.add_argument(
+        "path_to_pth",
+        type=Path,
+        help="path to pth file",
+    )
+    args = parser.parse_args()
+    get_loss_df(args.dataset_descriptor_file, args.path_to_pth).to_csv("loss.csv")
