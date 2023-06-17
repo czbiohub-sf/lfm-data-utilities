a9acab1d87f4f7b790a4eb7187d834ab61de8a84
Axel Jacobsen <axelnj44@gmail.com>
2023-06-16 21:32:55 -0700

    fix again for tiff, make ffmpeg compression smaller

--- lfm_data_utilities/model_evaluations/yogo/make_bbox_video.sh
+++ lfm_data_utilities/model_evaluations/yogo/make_bbox_video.sh
@@ -20,7 +20,7 @@ rm -f /tmp/temporary-yogo-images/*
 
 if [ -d "$2" ]; then
   # if path-to-images is a directory, check that it is only full of pngs
-  png_count=$(find "$2" -maxdepth 1 -type f -iname "*.png" | wc -l)
+  png_count=$(find "$2" -maxdepth 1 -type f \( -iname "*.png" -o -iname "*.tiff" \) | wc -l)
   if [ "$png_count" -eq 0 ]; then
     echo "There are no png files in the directory."
     exit 1
@@ -35,4 +35,4 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-ffmpeg -y -framerate 30 -pattern_type glob -i '/tmp/temporary-yogo-images/*.tiff' -c:v libx264 -r 30 -pix_fmt yuv420p "$out_fname" < /dev/null || exit
+ffmpeg -y -framerate 30 -pattern_type glob -i '/tmp/temporary-yogo-images/*.tiff' -c:v libx264 -r 30 -crf 18 -pix_fmt yuv420p "$out_fname" < /dev/null || exit
