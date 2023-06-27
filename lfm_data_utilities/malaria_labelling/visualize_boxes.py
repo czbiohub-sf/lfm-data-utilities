29566ecac5eb1d3f21a1d8d637a4bf78d24f3112
Axel Jacobsen <axelnj44@gmail.com>
2023-06-27 14:09:56 -0700

    fix visualize obes

--- lfm_data_utilities/malaria_labelling/visualize_boxes.py
+++ lfm_data_utilities/malaria_labelling/visualize_boxes.py
@@ -1,14 +1,19 @@
 #! /usr/bin/env python3
 
 import json
+import torch
 import signal
+import numpy as np
+from PIL import Image, ImageDraw
 
 import matplotlib.pyplot as plt
 
 from pathlib import Path
 from typing import Generator, Iterable, Tuple, Optional, Dict, Any
 
-from yogo.utils import draw_rects
+from yogo.data.dataset import YOGO_CLASS_ORDERING
+
+from yogo.utils.utils import bbox_colour
 from yogo.data.dataset import read_grayscale, load_labels
 
 from labelling_constants import CLASSES
@@ -26,14 +31,44 @@ def find_label_file(label_dir: Path, image_path: Path) -> Path:
     raise FileNotFoundError(f"label file not found for {str(image_path)}")
 
 
+def draw_rects(image_path, label_path):
+    pil_img = Image.open(image_path).convert("L")
+
+    notes_path = Path(label_path).parent.parent / "notes.json"
+    if notes_path.exists():
+        with open(notes_path) as f:
+            json_data = json.load(f)
+    else:
+        json_data = None
+
+    label_tensor = load_labels(label_path, notes_data=json_data)
+
+    rgb = Image.new("RGBA", pil_img.size)
+    rgb.paste(pil_img)
+
+    draw = ImageDraw.Draw(rgb)
+
+    img_w, img_h = pil_img.size
+    for r in label_tensor:
+        # convert cxcywh to xyxy and scale to image size
+        x1 = (r[1] - r[3] / 2) * img_w
+        y1 = (r[2] - r[4] / 2) * img_h
+        x2 = (r[1] + r[3] / 2) * img_w
+        y2 = (r[2] + r[4] / 2) * img_h
+        label = YOGO_CLASS_ORDERING[int(r[0])]
+        draw.rectangle(
+            (x1, y1, x2, y2),
+            outline=bbox_colour(label),
+        )
+        draw.text((x1, y2), f"label: {label}", (0, 0, 0, 255))
+
+    return rgb
+
+
 def plot_img_labels_pair(
-    image_path: Path, label_path: Path, notes_file: Optional[Dict[str, Any]]
+    image_path: Path, label_path: Path
 ):
-    labels = load_labels(label_path, dataset_classes=CLASSES)
-
-    img = read_grayscale(str(image_path)).squeeze()
-    annotated_img = draw_rects(img, labels, labels=CLASSES)
-
+    annotated_img = draw_rects(image_path, label_path)
     plt.imshow(annotated_img)
     plt.show()
 
