5479b2cd812e40542adc8d824782f4a3a096eb19
Axel Jacobsen <axelnj44@gmail.com>
2023-06-28 14:23:24 -0700

    argparser

--- lfm_data_utilities/model_evaluations/yogo/scaling/plot_incremental_data.py
+++ lfm_data_utilities/model_evaluations/yogo/scaling/plot_incremental_data.py
@@ -5,43 +5,54 @@ import matplotlib.pyplot as plt
 
 pl.Config.set_tbl_rows(100)
 
-q = pl.scan_csv("./wandb_export_2023-06-28T10 21 18.486-07 00.csv")
+if __name__ == "__main__":
+    import argparse
 
-cols = (
-    "Name",
-    "Notes",
-    "best_val_loss",
-    "classification_loss",
-    "iou_loss",
-    "objectnes_loss_no_obj",
-    "objectnes_loss_obj",
-    "test loss",
-    "test mAP",
-    "test precision",
-    "test recall",
-    "val loss",
-)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("csv", help="wandb csv export")
+    args = parser.parse_args()
 
-dataset_sizes = (
-    q.select("training set size")
-    .collect()
-    .apply(lambda g: int(g[0].replace(" images", "")))
-    .rename({"apply": "training set size"})  # probably hacky
-)
-rest = q.select(cols).collect()
-df = dataset_sizes.hstack(rest)
+    q = pl.scan_csv(args.csv)
 
-fig, ax = plt.subplots(2,1, figsize=(15, 10))
-fig.suptitle("Model Performance vs. Training Set Size")
+    cols = (
+        "Name",
+        "Notes",
+        "best_val_loss",
+        "classification_loss",
+        "iou_loss",
+        "objectnes_loss_no_obj",
+        "objectnes_loss_obj",
+        "test loss",
+        "test mAP",
+        "test precision",
+        "test recall",
+        "val loss",
+    )
 
-plotting_columns = (
-    ["test loss", "val loss"],
-    ["test mAP", "test precision", "test recall"],
-)
+    dataset_sizes = (
+        q.select("training set size")
+        .collect()
+        .apply(lambda g: int(g[0].replace(" images", "")))
+        .rename({"apply": "training set size"})  # probably hacky
+    )
+    rest = q.select(cols).collect()
+    df = dataset_sizes.hstack(rest)
 
-for i in range(2):
-    ax[i].plot(df.select("training set size"), df.select(plotting_columns[i]), label=plotting_columns[i])
-    ax[i].legend()
+    fig, ax = plt.subplots(2, 1, figsize=(15, 10))
+    fig.suptitle("Model Performance vs. Training Set Size")
 
-fig.tight_layout()
-plt.show()
+    plotting_columns = (
+        ["test loss", "val loss"],
+        ["test mAP", "test precision", "test recall"],
+    )
+
+    for i in range(2):
+        ax[i].plot(
+            df.select("training set size"),
+            df.select(plotting_columns[i]),
+            label=plotting_columns[i],
+        )
+        ax[i].legend()
+
+    fig.tight_layout()
+    plt.show()
