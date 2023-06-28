473323eaef07a33a8b171ced5651040e0e79d297
Axel Jacobsen <axelnj44@gmail.com>
2023-06-28 15:25:13 -0700

    OK i get polars now

--- lfm_data_utilities/model_evaluations/yogo/scaling/plot_incremental_data.py
+++ lfm_data_utilities/model_evaluations/yogo/scaling/plot_incremental_data.py
@@ -15,44 +15,60 @@ if __name__ == "__main__":
     q = pl.scan_csv(args.csv)
 
     cols = (
+        "training set size",
         "Name",
         "Notes",
         "best_val_loss",
-        "classification_loss",
-        "iou_loss",
-        "objectnes_loss_no_obj",
-        "objectnes_loss_obj",
         "test loss",
         "test mAP",
         "test precision",
         "test recall",
-        "val loss",
     )
 
-    dataset_sizes = (
-        q.select("training set size")
+    numerical_cols = (
+        "best_val_loss",
+        "test loss",
+        "test mAP",
+        "test precision",
+        "test recall",
+    )
+    sort_key = "training set size (numerical)"
+
+    df = (
+        q.select(
+            [
+                *cols,
+                pl.col("training set size")
+                    .str.replace(" images", "")
+                    .cast(pl.Int32)
+                    .alias(sort_key),
+            ]
+        )
         .collect()
-        .apply(lambda g: int(g[0].replace(" images", "")))
-        .rename({"apply": "training set size"})  # probably hacky
+        .groupby("Notes")
+        .agg(pl.col(c).mean() for c in (*numerical_cols, sort_key))
+        .sort(sort_key)
     )
-    rest = q.select(cols).collect()
-    df = dataset_sizes.hstack(rest)
-
-    fig, ax = plt.subplots(2, 1, figsize=(15, 10))
-    fig.suptitle("Model Performance vs. Training Set Size")
+    print(df)
 
     plotting_columns = (
-        ["test loss", "val loss"],
+        ["test loss", "best_val_loss"],
         ["test mAP", "test precision", "test recall"],
     )
 
-    for i in range(2):
-        ax[i].plot(
-            df.select("training set size"),
-            df.select(plotting_columns[i]),
-            label=plotting_columns[i],
-        )
+    fig, ax = plt.subplots(len(plotting_columns), 1, figsize=(10, 10))
+    fig.suptitle("Model Performance vs. Training Set Size")
+
+    for i in range(len(plotting_columns)):
+        for col in plotting_columns[i]:
+            ax[i].set_ylabel(col)
+            ax[i].plot(
+                df.select(sort_key),
+                df.select(col),
+                label=col,
+            )
         ax[i].legend()
 
+    plt.legend()
     fig.tight_layout()
     plt.show()
