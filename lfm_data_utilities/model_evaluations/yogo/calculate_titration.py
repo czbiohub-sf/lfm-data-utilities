1f98a697c9991079a8b8ecf171b10793f98d9b88
Axel Jacobsen <axelnj44@gmail.com>
2023-06-28 17:46:05 -0700

    add aspect ratio calc

--- lfm_data_utilities/model_evaluations/yogo/calculate_titration.py
+++ lfm_data_utilities/model_evaluations/yogo/calculate_titration.py
@@ -219,6 +219,9 @@ if __name__ == "__main__":
             path_to_images=path,
             batch_size=64,
             use_tqdm=True,
+            obj_thresh=0.5,
+            iou_thresh=0.5,
+            aspect_thresh=3,
             vertical_crop_height_px=(
                 round(772 * args.crop_height) if args.crop_height is not None else None
             ),
