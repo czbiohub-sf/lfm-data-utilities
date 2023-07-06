94a25a08aee253b59073d182fba7f79071611351
Axel Jacobsen <axelnj44@gmail.com>
2023-07-05 17:10:02 -0700

    much progress; need to convert fixed tasks.json files to yolo format

--- lfm_data_utilities/malaria_labelling/thumbnail_sort_labelling.py
+++ lfm_data_utilities/malaria_labelling/thumbnail_sort_labelling.py
@@ -29,7 +29,8 @@ from PIL import Image
 from tqdm import tqdm
 from pathlib import Path
 from datetime import datetime
-from typing import List, Dict, Optional
+from collections import defaultdict
+from typing import List, Dict, Optional, Tuple, DefaultDict
 
 from yogo.data.dataset import YOGO_CLASS_ORDERING
 from yogo.data.dataset_description_file import load_dataset_description
@@ -37,7 +38,7 @@ from lfm_data_utilities.malaria_labelling.generate_labelstudio_tasks import (
     gen_task,
     LFM_SCOPE_PATH,
 )
-from lfm_data_utilities.utils import path_is_relative_to
+from lfm_data_utilities.utils import path_is_relative_to, timing_context_manager
 
 
 DEFAULT_LABELS_PATH = Path(
@@ -85,10 +86,23 @@ def create_tasks_files_for_run_sets(
     return task_paths
 
 
+def create_thumbnail_name(class_: str, cell_id: str, task_json_id: str) -> str:
+    return f"{class_}_{cell_id}_{task_json_id}.png"
+
+def parse_thumbnail_name(thumbnail_name: str) -> Tuple[str, ...]:
+    """
+    parses a thumbnail name into class, cell_id, and task.json id
+
+    We can just remove the '.png' and split on '_', since class, cell_id, and task_json_id don't
+    have underscores in them.
+    """
+    return tuple(s.strip() for s in thumbnail_name.replace(".png", "").split("_"))
+
+
 def create_thumbnails_from_tasks_and_images(
-    tasks_json_path: Path, class_dirs: Dict[str, Path], run_name: Optional[str] = None
+    tasks_json_path: Path, class_dirs: Dict[str, Path], task_json_id: Optional[str] = None
 ):
-    run_name = run_name or tasks_json_path.parent.name
+    task_json_id = task_json_id or tasks_json_path.parent.name
 
     with open(tasks_json_path) as f:
         tasks = json.load(f)
@@ -122,7 +136,7 @@ def create_thumbnails_from_tasks_and_images(
 
             cell_image = image[y1:y2, x1:x2]
             pil_cell_image = Image.fromarray(cell_image)
-            pil_cell_image.save(class_dir / f"{class_}_{cell_id}_{run_name}.png")
+            pil_cell_image.save(class_dir / create_thumbnail_name(class_, cell_id, task_json_id))
 
 
 def create_thumbnails_for_sorting(
@@ -142,17 +156,15 @@ def create_thumbnails_for_sorting(
 
     N = int(math.log(len(task_paths), 10)) + 1
     id_to_task_path: Dict[str, str] = {}
-    for i, task_path in tqdm(enumerate(task_paths), desc="creating thumbnails"):
-        create_thumbnails_from_tasks_and_images(
-            task_path, class_dirs, run_name=f"{i:0{N}}"
-        )
+    for i, task_path in tqdm(enumerate(task_paths), total=len(task_paths), desc="creating thumbnails"):
+        create_thumbnails_from_tasks_and_images(task_path, class_dirs, task_json_id=f"{i:0{N}}")
         id_to_task_path[f"{i:0{N}}"] = str(task_path)
 
     with open(args.path_to_output_dir / "id_to_task_path.json", "w") as f:
         json.dump(id_to_task_path, f)
 
 
-def sort_thumbnails(path_to_thumbnails: Path):
+def sort_thumbnails(path_to_thumbnails: Path, dry_run=True):
     """
     The thumbnails dir should have three things:
         - a set of folders named after the classes ("Class Folder" from now on)
@@ -184,12 +196,68 @@ def sort_thumbnails(path_to_thumbnails: Path):
             )
 
     # create backup of vetted
-    vetted_backup_path = str(
-        DEFAULT_LABELS_PATH
-        / "vetted-backup"
-        / f"backup-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
-    )
-    shutil.make_archive(vetted_backup_path, "zip", DEFAULT_LABELS_PATH / "vetted")
+    with timing_context_manager(f"creating backup of {DEFAULT_LABELS_PATH / 'vetted'}"):
+        vetted_backup_path = str(
+            DEFAULT_LABELS_PATH
+            / "vetted-backup"
+            / f"backup-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+        )
+        if not dry_run
+            shutil.make_archive(vetted_backup_path, "zip", DEFAULT_LABELS_PATH / "vetted")
+
+    # create a list of all the corrections
+    id_to_list_of_corrections: DefaultDict[str, List[Dict[str, str]]] = defaultdict(list)
+    for class_ in YOGO_CLASS_ORDERING:
+        corrected_class_dir = path_to_thumbnails / f"corrected_{class_}"
+        for thumbnail in corrected_class_dir.iterdir():
+            original_class, cell_id, task_json_id = parse_thumbnail_name(thumbnail.name)
+
+            id_to_list_of_corrections[task_json_id].append({
+                "cell_id": cell_id,
+                "original_class": original_class,
+                "corrected_class": class_,
+            })
+
+    # Iterate through the corrections tasks-wise
+    # This is going to be horifically inefficient - label studio chose their
+    # tasks.json format poorly. They have list of dicts of predictions, and each
+    # dict has an id. Why not make it a dict of dicts, mapping the cell id to the
+    # prediction? It would turn the search from O(n) to O(1). Anyways, don't blame me.
+    for task_json_id, corrections in id_to_list_of_corrections.items():
+        if len(corrections) == 0:
+            continue
+
+        # read the json file
+        with open(id_to_task_path[task_json_id]) as f:
+            tasks = json.load(f)
+
+        for correction in corrections:
+            cell_id = correction["cell_id"]
+            original_class = correction["original_class"]
+            corrected_class = correction["corrected_class"]
+
+            # TODO maybe this n_corrections would be good for a sanity check?
+            n_corrections = 0
+            for i, image_prediction in enumerate(tasks):
+                bbox_predictions = image_prediction["predictions"][0]["result"]  # lol
+                for j, bbox_prediction in enumerate(bbox_predictions):
+                    if bbox_prediction["id"] == cell_id:
+                        bbox_prediction["value"]["rectanglelabels"] = [corrected_class]
+                        # axel's debugging print statements
+                        print(f"corrected {cell_id} from {original_class} to {corrected_class}")
+                        print(
+                            f"tasks['{i}']['predictions'][0]['result']['{j}']['value']['rectanglelabels'] = "
+                            f"{tasks[i]['predictions'][0]['result'][j]['value']['rectanglelabels']}"
+                        )
+                        n_corrections += 1
+
+            if n_corrections == 0:
+                raise ValueError(f"could not find cell_id {cell_id} in task {id_to_task_path[task_json_id]}")
+
+        # write the (corrected) json file
+        if not dry_run:
+            with open(id_to_task_path[task_json_id], "w") as f:
+                json.dump(tasks, f)
 
 
 if __name__ == "__main__":
