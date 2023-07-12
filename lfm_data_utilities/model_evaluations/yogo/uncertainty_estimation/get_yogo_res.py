29333d720f21b3ee85b8e849a8992cecf13f0ef5
Michelle Khoo <michelle.khoo@login-01.clusternet>
2023-07-12 16:25:10 -0700

    Add dataset def locally and save metadata

--- lfm_data_utilities/model_evaluations/yogo/uncertainty_estimation/get_yogo_res.py
+++ lfm_data_utilities/model_evaluations/yogo/uncertainty_estimation/get_yogo_res.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import subprocess
+import csv
 import torch
 import numpy as np
 
@@ -13,21 +15,36 @@ from lfm_data_utilities.model_evaluations.yogo.rank_yogo_loss import get_dataloa
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
     base_dir = Path("/hpc/projects/flexo/MicroscopyData/Bioengineering/LFM_scope/misc/yogo-statistics/")
     folder = Path(sys.argv[1])
     data_file = base_dir / folder / "res.csv"
+    
     if not os.path.exists(base_dir / folder):
         os.makedirs(base_dir / folder)
+    
+    model_file = "/hpc/projects/flexo/MicroscopyData/Bioengineering/LFM_scope/yogo_models/twilight-aardvark-1094/best.pth"
+    dataset_def_file = "dataset_defs/all-labelled-data.yml"
+ 
+    git_branch = subprocess.check_output(["git", "symbolic-ref", "--short", "HEAD"]).decode("ascii").strip()
+    git_commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
 
-    net, cfg = YOGO.from_pth(
-        "/hpc/projects/flexo/MicroscopyData/Bioengineering/LFM_scope/yogo_models/honest-sweep-51/best.pth",
-        inference=True,
-    )
+    metadata = {
+            'yogo_model' : model_file,
+            'dataset_def' : dataset_def_file,
+            'git_branch' : git_branch,
+            'git_commit' : git_commit,
+        }
+
+    with open(base_dir / folder / "metadata.csv", 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in metadata.items():
+            writer.writerow([key, value])
+
+    net, cfg = YOGO.from_pth(model_file, inference=True)
     net.to(device)
 
     dataloaders = get_dataloader(
-        "/hpc/projects/flexo/MicroscopyData/Bioengineering/LFM_scope/biohub-labels/dataset_defs/4verification-all-labelled-data-test.yml",
+        dataset_def_file,
         1,
         Sx=129,
         Sy=97,
@@ -64,5 +81,6 @@ if __name__ == '__main__':
 
             print(f"{img_count} - {img_path[0]}")
     
-    np.savetxt(data_file, all_res, delimiter=',')
-    print(f"Saved data to {data_file}")
+    np.savetxt(base_dir / folder / "res.csv", all_res, delimiter=',')
+    print(f"Saved data to {base_dir / folder}")
+
