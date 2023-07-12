3a4c13ca20f6ab9cffe92b44d0d798aa744df3f1
Axel Jacobsen <axelnj44@gmail.com>
2023-07-12 16:27:35 -0700

    black & ruff

--- lfm_data_utilities/model_evaluations/yogo/statistics/yogo_verification.py
+++ lfm_data_utilities/model_evaluations/yogo/statistics/yogo_verification.py
@@ -10,11 +10,12 @@ from yogo.metrics import Metrics
 from lfm_data_utilities.model_evaluations.yogo.rank_yogo_loss import get_dataloader
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-    base_dir = Path("/hpc/projects/flexo/MicroscopyData/Bioengineering/LFM_scope/misc/yogo-statistics/")
+    base_dir = Path(
+        "/hpc/projects/flexo/MicroscopyData/Bioengineering/LFM_scope/misc/yogo-statistics/"
+    )
     folder = Path(sys.argv[1])
     data_file = base_dir / folder / "res.csv"
     if not os.path.exists(base_dir / folder):
@@ -45,24 +46,25 @@ if __name__ == '__main__':
             num_preds = formatted_labels[0].shape[0]
 
             res = np.empty((num_preds, 2))
-            
-            for pred_count, (fpred, flabel) in enumerate(zip(formatted_preds[0], formatted_labels[0])):
+
+            for pred_count, (fpred, flabel) in enumerate(
+                zip(formatted_preds[0], formatted_labels[0])
+            ):
                 model_confidence = fpred[5:]
                 model_class = torch.argmax(model_confidence)
                 actual_class = int(flabel[5])
                 correct = model_class == actual_class
-                
+
                 res[pred_count] = [model_confidence[actual_class], correct]
 
                 # print(f"{correct} - ACTUAL {actual_class}, MODEL {model_class} ({model_confidence[model_class]})")
 
-            
             if img_count == 0:
                 all_res = res
             else:
                 all_res = np.concatenate((all_res, res), axis=0)
 
             print(img_count)
-    
-    np.savetxt(data_file, all_res, delimiter=',')
+
+    np.savetxt(data_file, all_res, delimiter=",")
     print(f"Saved data to {data_file}")
