a2c49f55e7650d4ab3b5bec0009e760eb89b9745
Michelle Khoo <michelle.khoo@login-01.clusternet>
2023-07-12 12:16:33 -0700

    Updated calibration script

--- lfm_data_utilities/model_evaluations/yogo/uncertainty_estimation/calibrate_uncertainty.py
+++ lfm_data_utilities/model_evaluations/yogo/uncertainty_estimation/calibrate_uncertainty.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from pathlib import Path
 
+from yogo.data import YOGO_CLASS_ORDERING
+
 
 def sigmoid(x, a, b):
     return 1.0 / (1.0 + np.exp(-a*(x-b)))
@@ -18,10 +20,9 @@ if __name__ == '__main__':
     
     all_res = np.loadtxt(data_file, delimiter=',').T
     
-    for class_idx in classes:
-        num_workers = min(len(os.sched_getaffinity(0)) // 2, 32)
+    for class_idx, class_name in enumerate(YOGO_CLASS_ORDERING):
         class_filter = all_res[2] == class_idx
-        class_res = all_res[0:1, class_filter]
+        class_res = all_res[0:2, class_filter]
 
         num_res = np.shape(class_res)[1]
 
@@ -29,26 +30,21 @@ if __name__ == '__main__':
         x = np.linspace(0, 1, num=101)
         y = sigmoid(x, popt[0], popt[1])
 
-        bin_count = 20
-        bins = np.linspace(0, 1, num=bin_count+1)
-        bin_avg = np.empty(bin_count)
-        bin_centers = np.empty(bin_count)
-        for idx in range(bin_count):
+        num_bins = 20
+        bins = np.linspace(0, 1, num=num_bins+1)
+        bin_avg = np.empty(num_bins)
+        bin_centers = np.empty(num_bins)
+        for idx in range(num_bins):
             bin_filt = np.logical_and(class_res[0] > bins[idx], class_res[0] < bins[idx+1])
             bin_avg[idx] = np.mean(class_res[1][bin_filt])
             bin_centers[idx] = (bins[idx] + bins[idx+1]) / 2.0
 
-        plt.scatter(class_res[0], class_res[1], label="Binarized result")
-        plt.plot(x, y, color="orange", label="Sigmoid fit")
-        plt.title(f"{class_name.upper(): A={popt[0]:.2f}, B={popt[1]:.2f}, N={num_res}")
-        plt.legend()
-        plt.savefig(base_dir / folder / f"sigmoid_{class_name.lower()}.png")
-
         plt.figure()
-        plt.scatter(class_res[0], class_res[1], label="Binarized result")
+        plt.scatter(class_res[0], class_res[1], label="Binarized result", s=5)
+        plt.plot(x, y, color="orange", label="Sigmoid fit")
+        plt.title(f"{class_name.upper()}: sig_A={popt[0]:.2f}, sig_B={popt[1]:.2f}, bins={num_bins},  N={num_res}")
         plt.plot(bin_centers, bin_avg, color='red', label="Binned average")
-        plt.title(f"{class_name.upper(): Bins={bin_count}, N={num_res}")
         plt.legend()
-        plt.savefig(base_dir / folder / f"bins_{class_name.lower()}.png")
+        plt.savefig(base_dir / folder / f"{class_name.lower()}.png")
        
     print(f"Saved data to {base_dir / folder}")
