3bd73a0952b3d857fb97ff74eaa2f833376ce479
Axel Jacobsen <axelnj44@gmail.com>
2023-07-19 12:42:21 -0700

    refactor thumbnail sorting into creating, sorting

---
+++ lfm_data_utilities/malaria_labelling/thumbnail_labelling/create_thumbnails.py
@@ -0,0 +1,171 @@
+#! /usr/bin/env python3
+
+import math
+import json
+import shutil
+import numpy as np
+
+from PIL import Image
+from tqdm import tqdm
+from pathlib import Path
+from typing import List, Dict, Optional
+
+from yogo.data import YOGO_CLASS_ORDERING
+from yogo.data.dataset_description_file import load_dataset_description
+
+from lfm_data_utilities.malaria_labelling.generate_labelstudio_tasks import (
+    gen_task,
+    LFM_SCOPE_PATH,
+)
+
+
+DEFAULT_LABELS_PATH = Path(
+    "/hpc/projects/flexo/MicroscopyData/Bioengineering/LFM_scope/biohub-labels/"
+)
+
+
+def create_folders_for_output_dir(
+    output_dir_path: Path, classes: List[str], force_overwrite: bool = False
+) -> Dict[str, Path]:
+    """creates the 'thumbnail-folder' above"""
+    class_dirs = {}
+    for class_ in classes:
+        class_dir = output_dir_path / class_
+        corrected_class_dir = output_dir_path / f"corrected_{class_}"
+
+        if force_overwrite:
+            if class_dir.exists():
+                shutil.rmtree(class_dir)
+            if corrected_class_dir.exists():
+                shutil.rmtree(corrected_class_dir)
+
+        class_dir.mkdir(exist_ok=True, parents=True)
+        corrected_class_dir.mkdir(exist_ok=True, parents=True)
+
+        class_dirs[class_] = class_dir
+    return class_dirs
+
+
+def create_tasks_files_for_run_sets(path_to_labelled_data_ddf: Path) -> List[Path]:
+    ddf = load_dataset_description(path_to_labelled_data_ddf)
+    dataset_paths = ddf.dataset_paths + (ddf.test_dataset_paths or [])
+
+    task_paths = []
+    for d in tqdm(dataset_paths):
+        image_path = d["image_path"]
+        label_path = d["label_path"]
+        task_file = gen_task(
+            folder_path=Path(label_path).parent,
+            images_dir_path=image_path,
+            label_dir_name=Path(label_path).name,
+            tasks_file_name="thumbnail_correction_task.json",
+        )
+        task_paths.append(task_file)
+    return task_paths
+
+
+def create_thumbnail_name(class_: str, cell_id: str, task_json_id: str) -> str:
+    return f"{class_}_{cell_id}_{task_json_id}.png"
+
+
+def write_thumbnail(
+    class_dir: Path,
+    thumbnail_file_name: str,
+    image: Image.Image,
+    max_num_files_per_subdir: int = 1000,
+):
+    """
+    write the thumbnail to the class_dir, being aware of the number of thumbnails in each dir,
+    and creating new subdirs if needed
+    """
+    dirs = [p for p in class_dir.iterdir() if p.is_dir()]
+
+    # if there are no subdirs, create one
+    if len(dirs) == 0:
+        (class_dir / "0").mkdir()
+        dirs.append(class_dir / "0")
+
+    # place the thumbnail in the first subdir that has space
+    for subdir in dirs:
+        num_files_in_subdir = len(list(subdir.iterdir()))
+        if num_files_in_subdir < max_num_files_per_subdir:
+            image.save(class_dir / subdir / thumbnail_file_name)
+            return
+
+    # there were no subdirs w/ space, so create a new one
+    # naive new dirname, but whatever
+    new_dirname = str(len(dirs))
+    (class_dir / new_dirname).mkdir()
+    image.save(class_dir / new_dirname / thumbnail_file_name)
+
+
+def create_thumbnails_from_tasks_and_images(
+    tasks_json_path: Path,
+    class_dirs: Dict[str, Path],
+    task_json_id: Optional[str] = None,
+):
+    task_json_id = task_json_id or tasks_json_path.parent.name
+
+    with open(tasks_json_path) as f:
+        tasks = json.load(f)
+
+    for task in tasks:
+        image_url = task["data"]["image"]
+
+        # task.json files hold image urls that are relative to LFM_scope
+        image_path = LFM_SCOPE_PATH / image_url.replace("http://localhost:8081/", "")
+        image = np.array(Image.open(image_path).convert("L"))
+
+        img_h, img_w = image.shape
+
+        for prediction in task["predictions"][0]["result"]:
+            cell_id = prediction["id"]
+            class_ = prediction["value"]["rectanglelabels"][0]
+            class_dir = class_dirs[class_]
+
+            x1 = prediction["value"]["x"] / 100
+            y1 = prediction["value"]["y"] / 100
+            w = prediction["value"]["width"] / 100
+            h = prediction["value"]["height"] / 100
+
+            x1 = max(round(x1 * img_w), 0)
+            y1 = max(round(y1 * img_h), 0)
+            x2 = min(round(x1 + w * img_w), img_w - 1)
+            y2 = min(round(y1 + h * img_h), img_h - 1)
+
+            if x1 == x2 or y1 == y2:
+                continue
+
+            cell_image = image[y1:y2, x1:x2]
+            pil_cell_image = Image.fromarray(cell_image)
+            write_thumbnail(
+                class_dir,
+                create_thumbnail_name(class_, cell_id, task_json_id),
+                pil_cell_image,
+            )
+
+
+def create_thumbnails_for_sorting(
+    path_to_output_dir: Path,
+    path_to_labelled_data_ddf: Path,
+    overwrite_previous_thumbnails: bool = False,
+):
+    class_dirs = create_folders_for_output_dir(
+        path_to_output_dir,
+        YOGO_CLASS_ORDERING,
+        force_overwrite=overwrite_previous_thumbnails,
+    )
+    task_paths = create_tasks_files_for_run_sets(path_to_labelled_data_ddf)
+
+    N = int(math.log(len(task_paths), 10)) + 1
+    id_to_task_path: Dict[str, str] = {}
+    for i, task_path in tqdm(
+        enumerate(task_paths), total=len(task_paths), desc="creating thumbnails"
+    ):
+        create_thumbnails_from_tasks_and_images(
+            task_path, class_dirs, task_json_id=f"{i:0{N}}"
+        )
+        id_to_task_path[f"{i:0{N}}"] = str(task_path)
+
+    with open(path_to_output_dir / "id_to_task_path.json", "w") as f:
+        json.dump(id_to_task_path, f)
---
+++ lfm_data_utilities/malaria_labelling/thumbnail_labelling/sort_thumbnails.py
@@ -0,0 +1,163 @@
+#! /usr/bin/env python3
+
+import json
+import shutil
+
+from pathlib import Path
+from datetime import datetime
+from collections import defaultdict
+from typing import List, Dict, Tuple, DefaultDict
+
+from yogo.data import YOGO_CLASS_ORDERING
+
+from lfm_data_utilities.utils import path_is_relative_to, timing_context_manager
+from lfm_data_utilities.malaria_labelling.labelling_constants import CLASSES
+from lfm_data_utilities.malaria_labelling.label_studio_converter.convert_ls_to_yolo import (
+    convert_ls_to_yolo,
+)
+
+
+DEFAULT_LABELS_PATH = Path(
+    "/hpc/projects/flexo/MicroscopyData/Bioengineering/LFM_scope/biohub-labels/"
+)
+
+
+def parse_thumbnail_name(thumbnail_name: str) -> Tuple[str, ...]:
+    """
+    parses a thumbnail name into class, cell_id, and task.json id
+
+    We can just remove the '.png' and split on '_', since class, cell_id, and task_json_id don't
+    have underscores in them.
+    """
+    return tuple(s.strip() for s in thumbnail_name.replace(".png", "").split("_"))
+
+
+def sort_thumbnails(path_to_thumbnails: Path, commit=True):
+    """
+    The thumbnails dir should have three things:
+        - a set of folders named after the classes ("Class Folder" from now on)
+        - a set of folders named "corrected_<class>" for each class ("Corrected Class Folder" from now on)
+        - a file named "id_to_task_path.json" which maps task ids to task.json files
+
+    After creation, each thumbnail has the name <class>_<cell_id>_<id>.png. `id` is the id of the task.json file,
+    which one can get from id_to_task_path.json. `cell_id` is the id of the cell in that tasks.json. `class` is
+    the cell's original class.
+
+    The labeller will sort the thumbnails into the "Corrected Class Folder" for the class that they deem is the
+    true class for that cell.
+
+    This function will first create a backup of `LFM_scope/biohub-labels/vetted` into `LFM_scope/biohub-labels/vetted-backup`.
+    Then, it goes through the corrected class folders and will correct the tasks.json files. It will then export the
+
+    TODO hard coding DEFAULT_LABELS_PATH. What should we do with it? All the tasks.json paths should be from there, so this
+    information is redundant. Maybe we will use this to verify that the tasks.json files are correct? Need to reconsider this.
+    """
+    with open(path_to_thumbnails / "id_to_task_path.json") as f:
+        id_to_task_path = json.load(f)
+        id_to_task_path = {k: Path(v) for k, v in id_to_task_path.items()}
+
+    for task_path in id_to_task_path.values():
+        if not task_path.exists():
+            raise ValueError(f"task_path {task_path} does not exist")
+        elif not path_is_relative_to(task_path, DEFAULT_LABELS_PATH):
+            raise ValueError(
+                f"task_path {task_path} is not relative to {DEFAULT_LABELS_PATH}"
+            )
+
+    # create backup of vetted
+    with timing_context_manager(f"creating backup of {DEFAULT_LABELS_PATH / 'vetted'}"):
+        vetted_backup_path = str(
+            DEFAULT_LABELS_PATH
+            / "vetted-backup"
+            / f"backup-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+        )
+        if commit:
+            shutil.make_archive(
+                vetted_backup_path, "zip", DEFAULT_LABELS_PATH / "vetted"
+            )
+
+    # create a list of all the corrections
+    id_to_list_of_corrections: DefaultDict[str, List[Dict[str, str]]] = defaultdict(
+        list
+    )
+    for class_ in YOGO_CLASS_ORDERING:
+        corrected_class_dir = path_to_thumbnails / f"corrected_{class_}"
+        for thumbnail in corrected_class_dir.iterdir():
+            original_class, cell_id, task_json_id = parse_thumbnail_name(thumbnail.name)
+
+            id_to_list_of_corrections[task_json_id].append(
+                {
+                    "cell_id": cell_id,
+                    "original_class": original_class,
+                    "corrected_class": class_,
+                }
+            )
+
+    # Iterate through the corrections tasks-wise
+    # This is going to be horifically inefficient - label studio chose their
+    # tasks.json format poorly. They have list of dicts of predictions, and each
+    # dict has an id. Why not make it a dict of dicts, mapping the cell id to the
+    # prediction? It would turn the cell search from O(n) to O(1).
+    not_corrected = was_corrected = 0
+    for task_json_id, corrections in id_to_list_of_corrections.items():
+        if len(corrections) == 0:
+            continue
+
+        # read the json file
+        with open(id_to_task_path[task_json_id]) as f:
+            tasks = json.load(f)
+
+        for correction in corrections:
+            cell_id = correction["cell_id"]
+            original_class = correction["original_class"]
+            corrected_class = correction["corrected_class"]
+
+            corrected = False
+            for i, image_prediction in enumerate(tasks):
+                bbox_predictions = image_prediction["predictions"][0]["result"]  # lol
+                for j, bbox_prediction in enumerate(bbox_predictions):
+                    if bbox_prediction["id"] == cell_id:
+                        bbox_prediction["value"]["rectanglelabels"] = [corrected_class]
+                        # look at black's formatting here! geez!
+                        assert (
+                            tasks[i]["predictions"][0]["result"][j]["value"][
+                                "rectanglelabels"
+                            ][0]
+                            == corrected_class
+                        )
+                        corrected = True
+                        break
+
+            if not corrected:
+                # raise ValueError(
+                not_corrected += 1
+                print(
+                    f"could not find cell_id {cell_id} in task {id_to_task_path[task_json_id]}"
+                )
+            else:
+                was_corrected += 1
+
+        # write the (corrected) json file
+        if commit:
+            with open(id_to_task_path[task_json_id], "w") as f:
+                json.dump(tasks, f)
+        else:
+            print(
+                "would have written corrected tasks.json "
+                f"file to {id_to_task_path[task_json_id]}"
+            )
+
+    print(f"not corrected: {not_corrected}, was corrected: {was_corrected}")
+
+    # convert the corrected json files to yolo format
+    for task_path in id_to_task_path.values():
+        if commit:
+            convert_ls_to_yolo(
+                path_to_ls_file=task_path,
+                path_to_output_dir=task_path.parent,
+                classes=CLASSES,
+                overwrite_existing_labels=commit,
+                download_images=False,
+            )
+        else:
+            print(f"would have overwritten YOGO labels at {task_path.parent}")
