e1358e2378a2291d66f827716f30d59ccb58ad80
Axel Jacobsen <axelnj44@gmail.com>
2023-07-25 14:33:37 -0700

    mail me when these long-running jobs end

--- lfm_data_utilities/malaria_labelling/submit_cmd_gpu.sh
+++ lfm_data_utilities/malaria_labelling/submit_cmd_gpu.sh
@@ -8,6 +8,8 @@
 #SBATCH --gpus-per-node=a100:1
 #SBATCH --cpus-per-task=32
 #SBATCH --output=./slurm-outputs/%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=axel.jacobsen@czbiohub.org
 
 env | grep "^SLURM" | sort
 
