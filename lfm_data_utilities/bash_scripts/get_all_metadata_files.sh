dc3536d120f32e1aba4f338a0b0342f845fc2be1
i-jey <ilakkiyan.jeyakumar@gmail.com>
2023-10-17 15:21:25 -0700

    clean up + inform user of parameter to pass in

--- lfm_data_utilities/bash_scripts/get_all_metadata_files.sh
+++ lfm_data_utilities/bash_scripts/get_all_metadata_files.sh
@@ -1,13 +1,19 @@
 set -e
 
-echo "Usage:"
+echo "USAGE"
+echo -e "=================================="
 echo "./get_all_metadata_files.sh path_to_save"
+echo "e.g: ./get_all_metadata_files.sh /hpc/mydata/first.last"
+echo "=================================="
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. Please provide a path to save the folder, e.g "/hpc/mydata/first.last""
+    exit 1
+fi
+
 
 TLD="/hpc/projects/group.bioengineering/LFM_scope"
-echo "This script will search through the following folders: Uganda_full, 
-Uganda_full_2, Uganda_full_3. It will copy all the perimage metadata files 
-into a folder here ("all_metadata")."
-
-echo -e "Note: this script uses 'fd', a modern version of 'find'. The HPC should already have fd installed."
+echo -e "\nThis script will search through the following folders: Uganda_full, Uganda_full_2, Uganda_full_3. It will copy all the perimage metadata files into a folder called "all_metadata" in the location you specified."
 mkdir -p $1/all_metadata
 fd -e csv -g "*perimage*" $TLD/Uganda_full $TLD/Uganda_full_2 $TLD/Uganda_full_3 | xargs -I{} readlink -f {} | xargs -I{} cp {} $1/all_metadata
