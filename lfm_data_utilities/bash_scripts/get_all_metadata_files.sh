7923917e40c4b0772ace93f0f885b2994a8a790f
i-jey <ilakkiyan.jeyakumar@gmail.com>
2023-10-17 15:14:16 -0700

    user defined save location

--- lfm_data_utilities/bash_scripts/get_all_metadata_files.sh
+++ lfm_data_utilities/bash_scripts/get_all_metadata_files.sh
@@ -1,10 +1,13 @@
 set -e
 
+echo "Usage:"
+echo "./get_all_metadata_files.sh path_to_save"
+
 TLD="/hpc/projects/group.bioengineering/LFM_scope"
 echo "This script will search through the following folders: Uganda_full, 
 Uganda_full_2, Uganda_full_3. It will copy all the perimage metadata files 
 into a folder here ("all_metadata")."
 
 echo -e "Note: this script uses 'fd', a modern version of 'find'. The HPC should already have fd installed."
-mkdir -p all_metadata
-fd -e csv -g "*perimage*" $TLD/Uganda_full $TLD/Uganda_full_2 $TLD/Uganda_full_3 | xargs -I{} readlink -f {} | xargs -I{} cp {} all_metadata
+mkdir -p $1/all_metadata
+fd -e csv -g "*perimage*" $TLD/Uganda_full $TLD/Uganda_full_2 $TLD/Uganda_full_3 | xargs -I{} readlink -f {} | xargs -I{} cp {} $1/all_metadata
