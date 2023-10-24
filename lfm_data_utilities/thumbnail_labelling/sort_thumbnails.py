27aa69447dd7ca9914abe1d1f77af26123d5833c
Axel Jacobsen <axelnj44@gmail.com>
2023-10-24 14:06:14 -0700

    fix path reporting

--- lfm_data_utilities/thumbnail_labelling/sort_thumbnails.py
+++ lfm_data_utilities/thumbnail_labelling/sort_thumbnails.py
@@ -243,4 +243,4 @@ def sort_thumbnails(
                 download_images=False,
             )
         elif verbose:
-            print(f"would have overwritten YOGO labels at {task_path.parent}")
+            print(f"would have overwritten YOGO labels at {label_path.parent.resolve()}")
