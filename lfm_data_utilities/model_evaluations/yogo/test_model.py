c41dc7523318434d42f0ada3ce23e9972151a137
Axel Jacobsen <axelnj44@gmail.com>
2023-12-08 15:52:03 -0800

    ddp for test_model

--- lfm_data_utilities/model_evaluations/yogo/test_model.py
+++ lfm_data_utilities/model_evaluations/yogo/test_model.py
@@ -1,11 +1,15 @@
 #! /usr/bin/env python3
 
+import os
 import torch
 import wandb
 import argparse
 
 from pathlib import Path
+from typing import Iterable
 
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data import Dataset, ConcatDataset, DataLoader
 
 from yogo.model import YOGO
@@ -17,28 +21,38 @@ from yogo.data.yogo_dataloader import (
     choose_dataloader_num_workers,
     collate_batch,
 )
+from yogo.utils import (
+    draw_yogo_prediction,
+    get_wandb_confusion,
+    get_wandb_roc,
+    get_free_port,
+    choose_device,
+)
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("pth_path", type=Path)
-    parser.add_argument("dataset_defn_path", type=Path)
-    args = parser.parse_args()
-
+def test_model(rank: int, world_size: int, args: argparse.Namespace) -> None:
     y, cfg = YOGO.from_pth(args.pth_path, inference=False)
     y.to("cuda")
 
     dataloaders = get_dataloader(
         args.dataset_defn_path,
-        16,
+        64,
         y.get_grid_size()[0],
         y.get_grid_size()[1],
         normalize_images=cfg["normalize_images"],
     )
 
+    torch.distributed.init_process_group(
+        backend="nccl", rank=rank, world_size=world_size
+    )
+
     # take test and val since neither have been trained on and more data == gooder
     test_dataset: Dataset[ObjectDetectionDataset] = ConcatDataset(
         [dataloaders["val"].dataset, dataloaders["test"].dataset]
+    )
+    sampler: Iterable = DistributedSampler(
+        test_dataset,
+        rank=rank,
+        num_replicas=world_size,
     )
 
     num_workers = choose_dataloader_num_workers(len(test_dataset))  # type: ignore
@@ -48,7 +62,7 @@ if __name__ == "__main__":
         shuffle=False,
         drop_last=False,
         pin_memory=True,
-        batch_size=16,
+        batch_size=64,
         num_workers=num_workers,
         persistent_workers=num_workers > 0,
         generator=torch.Generator().manual_seed(111111),
@@ -83,3 +97,23 @@ if __name__ == "__main__":
         y,
     )
     Trainer._log_test_metrics(*test_metrics)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pth_path", type=Path)
+    parser.add_argument("dataset_defn_path", type=Path)
+    args = parser.parse_args()
+
+    world_size = torch.cuda.device_count()
+    if world_size == 0:
+        raise RuntimeError(
+            "at least 1 gpu is required for testing (otherwise it's painfully slow); "
+            "if cpu training is required, we can add it back"
+        )
+
+    os.environ["MASTER_ADDR"] = "0.0.0.0"
+    os.environ["MASTER_PORT"] = str(get_free_port())
+    mp.spawn(
+        test_model, args=(world_size, args), nprocs=world_size, join=True
+    )
