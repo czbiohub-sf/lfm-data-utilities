39e7faaace3204effe49dff159700ea4353878ee
Axel Jacobsen <axelnj44@gmail.com>
2024-01-25 12:17:58 -0800

    speed up heatmap_array_gen.sh with preempted node and no limits on array

--- lfm_data_utilities/model_evaluations/yogo/heatmap_array_gen.sh
+++ lfm_data_utilities/model_evaluations/yogo/heatmap_array_gen.sh
@@ -3,29 +3,31 @@
 #SBATCH --job-name=heatmapGeneration
 #SBATCH --output=slurm-outputs/array/%A_%a.out
 #SBATCH --error=slurm-outputs/array/%A_%a.err
-#SBATCH --time=24:00:00
+#SBATCH --time=00:20:00
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --array=1-1022%16
-#SBATCH --cpus-per-task=64
-#SBATCH --partition=gpu
+#SBATCH --array=1-1022
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=preempted
 #SBATCH --gpus-per-node=1
 
 
-if [ -z "$1" ]; then
+if [ "$#" -lt 2 ]; then
   echo "No path to list of zarr files provided"
-  echo "usage: $0 path-to-zarr-list"
+  echo "usage: $0 path-to-zarr-list yogo-model-name"
   exit 1
 fi
 
-ZARR_PATH=$(fd zip $(sed -n "$SLURM_ARRAY_TASK_ID"p "$1") -1)
+# ZARR_PATH=$(fd zip $(sed -n "$SLURM_ARRAY_TASK_ID"p "$1") -1)
+ZARR_PATH=$(sed -n "$SLURM_ARRAY_TASK_ID"p "$1")
+YOGO_MODEL_NAME="$2"
 
 echo "ZARR_PATH: $ZARR_PATH"
 
 out=$(
   ./create_heatmaps_and_masks.py \
-    ~/celldiagnosis/yogo/trained_models/confused-river-1979/best.pth \
-    /hpc/projects/group.bioengineering/LFM_scope/Uganda_heatmaps/confused-river-1979 \
+    ~/celldiagnosis/yogo/trained_models/$YOGO_MODEL_NAME/best.pth \
+    /hpc/projects/group.bioengineering/LFM_scope/Uganda_heatmaps/$YOGO_MODEL_NAME \
     --target-zip "$ZARR_PATH" \
 )
 
