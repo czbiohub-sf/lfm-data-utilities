d2470ca1c2aaebc1956e2e26185321165690fd74
Axel Jacobsen <axelnj44@gmail.com>
2024-02-17 13:17:06 -0800

    format + typing fixing

--- lfm_data_utilities/thumbnails/get_thumbnails_from_npy.py
+++ lfm_data_utilities/thumbnails/get_thumbnails_from_npy.py
@@ -8,27 +8,33 @@ from tqdm import tqdm
 import cv2
 import numpy as np
 
+
 def filter_for_conf(arr, conf_thresh):
     return arr[:, arr[7, :] > conf_thresh]
 
+
 def parse_filepaths(filepath):
-    if filepath.endswith('.txt'):
-        with open(filepath, 'r') as file:
+    if filepath.endswith(".txt"):
+        with open(filepath, "r") as file:
             lines = file.readlines()
             filepaths = [Path(line.strip()) for line in lines]
-    elif filepath.endswith('.npy'):
+    elif filepath.endswith(".npy"):
         filepaths = [Path(filepath)]
     else:
         raise ValueError("Invalid file format. Only .txt and .npy files are supported.")
-    
+
     return filepaths
+
 
 def get_imgs_dir(npy_file: Path) -> Path:
     return npy_file.parent.parent.parent / "images"
 
-def get_thumbnail_coords(npy_filepath: Path) -> List[Tuple[Path, Tuple[int, int, int, int]]]:
+
+def get_thumbnail_coords(
+    npy_filepath: Path,
+) -> List[Tuple[int, Path, Tuple[int, int, int, int]]]:
     """
-    Get the corresponding image file path and 
+    Get the corresponding image file path and
     coordinates of the parasitic cells in the .npy file.
     """
     imgs_dir = get_imgs_dir(npy_filepath)
@@ -49,7 +55,7 @@ def get_thumbnail_coords(npy_filepath: Path) -> List[Tuple[Path, Tuple[int, int,
     class_paths_and_coords = []
     for c in classes:
         # Skip healthy
-        if not c in [1, 2, 3, 4]:
+        if c not in [1, 2, 3, 4]:
             continue
 
         class_filter = arr[:, arr[6, :] == c]
@@ -59,10 +65,13 @@ def get_thumbnail_coords(npy_filepath: Path) -> List[Tuple[Path, Tuple[int, int,
             tlx, tly, brx, bry = [int(x) for x in class_filter[1:5, i]]
 
             class_paths_and_coords.append((int(c), img_path, (tlx, tly, brx, bry)))
-    
+
     return class_paths_and_coords
 
-def crop_and_save_thumbnail(path_and_coords: Tuple[int, Path, int, int, int, int], output_dir: Path):
+
+def crop_and_save_thumbnail(
+    path_and_coords: Tuple[int, Path, Tuple[int, int, int, int]], output_dir: Path
+):
     yogo_class, img_path, (tlx, tly, brx, bry) = path_and_coords
     img = cv2.imread(str(img_path))
     cropped_img = img[tly:bry, tlx:brx]
@@ -74,21 +83,37 @@ def crop_and_save_thumbnail(path_and_coords: Tuple[int, Path, int, int, int, int
         # Dimensions of the thumbnail are 0
         pass
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Process filepaths.')
-    parser.add_argument('filepath', type=str, help='Path to a .npy file or a .txt file containing filepaths')
-    parser.add_argument('output_dir', type=str, help='Path to the output directory')
+    parser = argparse.ArgumentParser(description="Process filepaths.")
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="Path to a .npy file or a .txt file containing filepaths",
+    )
+    parser.add_argument("output_dir", type=str, help="Path to the output directory")
     args = parser.parse_args()
-    
+
     filepaths = parse_filepaths(args.filepath)
 
-    paths_and_coords = [get_thumbnail_coords(x) for x in tqdm(filepaths, desc="Loading files")]
+    paths_and_coords = [
+        get_thumbnail_coords(x) for x in tqdm(filepaths, desc="Loading files")
+    ]
     paths_and_coords = [item for sublist in paths_and_coords for item in sublist]
 
-    partial_crop_and_save_thumbnail = partial(crop_and_save_thumbnail, output_dir=Path(args.output_dir))
-    
-    with Pool() as pool:
-        list(tqdm(pool.imap(partial_crop_and_save_thumbnail, paths_and_coords), desc="Saving thumbnails", total=len(paths_and_coords)))
+    partial_crop_and_save_thumbnail = partial(
+        crop_and_save_thumbnail, output_dir=Path(args.output_dir)
+    )
 
-if __name__ == '__main__':
+    with Pool() as pool:
+        list(
+            tqdm(
+                pool.imap(partial_crop_and_save_thumbnail, paths_and_coords),
+                desc="Saving thumbnails",
+                total=len(paths_and_coords),
+            )
+        )
+
+
+if __name__ == "__main__":
     main()
