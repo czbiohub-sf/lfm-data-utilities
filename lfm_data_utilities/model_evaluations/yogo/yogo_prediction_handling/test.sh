285413f01730bf5a507a33514bf29c7c963cbf9a
Axel Jacobsen <axelnj44@gmail.com>
2024-03-12 15:07:33 -0700

    update test!

--- lfm_data_utilities/model_evaluations/yogo/yogo_prediction_handling/test.sh
+++ lfm_data_utilities/model_evaluations/yogo/yogo_prediction_handling/test.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 
-
 if [ ! -d "venv" ]; then
   # create virtual environment
   echo "creating virtual environment..."
@@ -11,7 +10,23 @@ fi
 echo "activating virtual environment..."
 source venv/bin/activate
 
-echo "installing yogo..."
-python3 -m pip install git+ssh://git@github.com/czbiohub-sf/yogo.git@prediction-handling-v2#egg=yogo
+# if we can't find `yogo` in `python3 -m pip freeze`, install it with deps
+# otherwise, just update it with --no-deps
+if [ ! -f "venv/bin/yogo" ]; then
+  echo "installing yogo..."
+  python3 -m pip install --force-reinstall "git+ssh://git@github.com/czbiohub-sf/yogo.git@prediction-handling-v2#egg=yogo" > /dev/null
+else
+  echo "updating yogo..."
+  python3 -m pip install --force-reinstall --no-deps "git+ssh://git@github.com/czbiohub-sf/yogo.git@prediction-handling-v2#egg=yogo" > /dev/null
+fi
+
+model_path=~/celldiagnosis/yogo/trained_models/absurd-moon-607/best.pth
+data_path=~/celldiagnosis/dataset_defs/human-labels/all-dataset-subsets-no-aug.yml
+
+srun --partition=gpu --gres=gpu:1 yogo test $model_path $data_path --wandb --include-mAP --tags handling-test default --PREDICTION-FORMATTER default &
+srun --partition=gpu --gres=gpu:1 yogo test $model_path $data_path --wandb --include-mAP --tags handling-test opt_A --PREDICTION-FORMATTER opt_A &
+srun --partition=gpu --gres=gpu:1 yogo test $model_path $data_path --wandb --include-mAP --tags handling-test opt_B --PREDICTION-FORMATTER opt_B &
+
+wait
 
 deactivate
