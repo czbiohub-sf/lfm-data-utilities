aa80a2f22f73a1a813aeadfc5562e6533262aa8f
Axel Jacobsen <axelnj44@gmail.com>
2024-05-22 14:24:10 -0700

    update layer-mosaic to support tiff files

--- lfm_data_utilities/model_evaluations/yogo/layer-mosaic.py
+++ lfm_data_utilities/model_evaluations/yogo/layer-mosaic.py
@@ -1,26 +1,18 @@
-import torch
-import torch.nn as nn
 import matplotlib.pyplot as plt
-from pathlib import Path
-from yogo.model import YOGO
-from yogo.data.utils import read_image
+
 from matplotlib.backends.backend_pdf import PdfPages
 
+import torch
+import torch.nn as nn
+import tifffile as tiff
 
-def save_feature_maps_to_pdf(feature_maps, output_path, layer_num):
-    n, c, h, w = feature_maps.shape
+from pathlib import Path
 
-    with PdfPages(output_path) as pdf:
-        for i in range(c):
-            fig, ax = plt.subplots(figsize=(8, 8))
-            ax.imshow(feature_maps[0, i].detach().cpu().numpy(), cmap="gray")
-            ax.axis("off")
-            plt.title(f"Layer {layer_num} - Feature Map {i+1}")
-            pdf.savefig(fig)
-            plt.close(fig)
+from yogo.model import YOGO
+from yogo.data.utils import read_image
 
 
-def save_layer_feature_maps_to_pdf(model, image_path, output_path, layer_num):
+def get_feature_maps(model, image_path, layer_num):
     layers = []
     for i in range(layer_num):
         layers.append(list(model.model.children())[i])
@@ -35,7 +27,28 @@ def save_layer_feature_maps_to_pdf(model, image_path, output_path, layer_num):
     with torch.no_grad():
         feature_maps = selected_layers(image)
 
-    save_feature_maps_to_pdf(feature_maps, output_path, layer_num)
+    return feature_maps
+
+
+def save_feature_maps_to_pdf(feature_maps, output_path, layer_num):
+    n, c, h, w = feature_maps.shape
+
+    with PdfPages(output_path) as pdf:
+        for i in range(c):
+            fig, ax = plt.subplots(figsize=(8, 8))
+            cax = ax.imshow(feature_maps[0, i].detach().cpu().numpy(), cmap="gray")
+            ax.axis("off")
+            plt.title(f"Layer {layer_num} - Feature Map {i+1}")
+            fig.colorbar(cax, ax=ax, orientation="vertical")
+            pdf.savefig(fig)
+            plt.close(fig)
+
+
+def save_feature_maps_to_tiff(feature_maps, output_path, layer_num):
+    _, c, h, w = feature_maps.shape
+    feature_maps_np = feature_maps[0].detach().cpu().numpy()
+    tiff_path = output_path.parent / f"feature_maps_layer_{layer_num}.tiff"
+    tiff.imwrite(tiff_path, feature_maps_np)
 
 
 if __name__ == "__main__":
@@ -45,6 +58,17 @@ if __name__ == "__main__":
     parser.add_argument("image", type=str)
     parser.add_argument("pth_path", type=str)
     parser.add_argument("--output-dir", default=".", type=Path)
+    output_group = parser.add_mutually_exclusive_group(required=True)
+    output_group.add_argument(
+        "--pdf",
+        help="save outputs as images in pdfs; useful for just browsing",
+        action=argparse.BooleanOptionalAction,
+    )
+    output_group.add_argument(
+        "--tiff",
+        help="save outputs as tiff files; each layer's outputs will be one tiff file",
+        action=argparse.BooleanOptionalAction,
+    )
     args = parser.parse_args()
 
     y, cfg = YOGO.from_pth(
@@ -54,5 +78,13 @@ if __name__ == "__main__":
 
     num_layers = len(y.model)
     for layer_num in range(1, num_layers + 1):
+        feature_maps = get_feature_maps(y, args.image, layer_num)
+
         output_path = args.output_dir / f"feature_maps_layer_{layer_num}.pdf"
-        save_layer_feature_maps_to_pdf(y, args.image, output_path, layer_num)
+
+        if args.pdf:
+            save_feature_maps_to_pdf(feature_maps, output_path, layer_num)
+        elif args.tiff:
+            save_feature_maps_to_tiff(feature_maps, output_path, layer_num)
+        else:
+            raise NotImplementedError("should not happen!")
