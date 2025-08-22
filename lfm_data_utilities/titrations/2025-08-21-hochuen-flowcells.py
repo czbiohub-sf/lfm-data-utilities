9adb788d9bd906c99a14d255f5d8e3a6847bbb2a
Michelle Khoo <michelle.khoo@czbiohub.org>
2025-08-22 11:19:44 -0700

    Added hochuen flow cell titration processing

---
+++ lfm_data_utilities/titrations/2025-08-21-hochuen-flowcells.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+parasitemias = [
+    200000,
+    100000,
+    50000,
+    25000,
+    12500,
+    6250,
+    3125,
+    0,
+]
+
+run1 = [
+    17489,
+    17345,
+    20994,
+    10863,
+    3840,
+    1280,
+    162,
+    0,
+]
+
+run2 = [
+    np.nan,
+    np.nan,
+    8522,
+    7047,
+    2509,
+    2043,
+    400,
+    0,
+]
+
+yticks = [
+    40000, 20000, 10000, 5000, 2500, 1250, 750, 325, 162,
+]
+
+other = np.array([
+    [25000, 3369],
+    [25000, 5796],
+])
+
+plt.yscale('log', base=2)
+plt.xscale('log', base=2)
+plt.xticks(parasitemias[:-1], parasitemias[:-1])
+plt.yticks(yticks, yticks)
+
+plt.title("2025-08-21 Titration")
+plt.xlim([1000, 250000])
+plt.ylim([100, 50000])
+
+plt.plot(parasitemias, run1, label="Run 1")
+plt.plot(parasitemias, run2, label="Run 2")
+plt.scatter(other[:, 0], other[:, 1], label="Repeatability", marker='x', c='r')
+plt.legend()
+plt.show()
