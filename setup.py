7662a37f807d1b88759a3ea38d724f050293d3b6
Ilakkiyan Jeyakumar <ilakkiyan.jeyakumar@gmail.com>
2025-12-05 07:44:58 -0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lfm_data_utilities",
+    name="lfm-data-utilities",
     description="Tools for processing data related to the lfm-scope (ulc-malaria-scope)",
     author="CZ Biohub | Bioengineering",
     author_email="paul.lebel@czbiohub.org",
-    packages=find_packages("lfm-data-utilities"),
     install_requires=[
         "AllanTools==2019.9",
         "bridson==0.1.0",
         "GitPython==3.1.37",
         "matplotlib==3.5.3",
         "nbdev",
-        "numpy==1.24.2",
+        "numcodecs==0.15.1",
+        "numpy<2.0",
         "opencv-python>=4.7.0.72",
         "pandas>=1.5.3",
         "tqdm>=4.64.1",
